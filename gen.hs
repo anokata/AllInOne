85e9e6e8c9d50f5bce352cc74eae1f47882506fb
anokata <quartenium@gmail.com>
2014-06-12 16:55:36 +0400

    change to generic type MapElem

--- gen.hs
+++ gen.hs
@@ -5,21 +5,26 @@ import System.IO
 -- карты могут быть разные (карта местности, объектов) различающиеся поведением но имеющие общее.
 -- значит будут наследоваться от общей
 -- локальная карта(чего угодно). фиксированного размера? 
+data MapElem = MapChar Char | MapNone
 data Localmap a = Lmap [[a]]
 
 instance Show a => Show (Localmap a) where
 	show (Lmap x) = concatMap (concatMap show) x
-
+{-
 data MapChar = MapChar Char
 instance Show MapChar where
 	show (MapChar x) = [x]
+-}
+instance Show MapElem where
+	show (MapChar x) = [x]
+	show MapNone = [' ']
 
-stringTolistMapChar :: String -> [MapChar]
+stringTolistMapChar :: String -> [MapElem]
 stringTolistMapChar [] = []
 stringTolistMapChar (x:y) = MapChar x : stringTolistMapChar y
 
 -- простейшая карта с объектами символами
-data CharLocalmap = CharLocalmap (Localmap MapChar) 
+data CharLocalmap = CharLocalmap (Localmap MapElem) 
 -- даже почти не надо доопределять show?
 instance Show CharLocalmap where
 	show (CharLocalmap x) = show x
@@ -31,6 +36,10 @@ inputMapFromStrings [[]] = CharLocalmap $ Lmap [[]]
 -- обработаем каждую строку - элемент списка и результат в список типа Lmap - concatMap
 inputMapFromStrings x = CharLocalmap $ Lmap $ map stringTolistMapChar x
 
+-- теперь сделаю более сложные типы объектов
+
+-- а также их преобразование из специально построенных строк описывающих карту
+-- допустим так: "{item: '$';  map = ["   $  "," $$   "] "
 
 main = do 
 {-	print "#####"
