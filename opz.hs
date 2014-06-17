f1b12a9c373c87ff112584f68721972c7aa74b7e
anokata <quartenium@gmail.com>
2014-06-17 21:32:40 +0400

    работа над калк опз

--- opz.hs
+++ opz.hs
@@ -1,13 +1,33 @@
 #!/usr/bin/env runhaskell
 import Prelude hiding (catch)
 import Data.Either
+--import qualified Data.Map as M
 
 test1 = "10 4 3 + 2 * -"
 
 main = do
     print $ calculate test1 --input
 
-calculate :: String -> Either String Int
+calculate :: String -> Either String Double
 calculate [] = Left "Empty!"
+calculate x = let res = foldl dispatch emptyStack (words x) in
+                case res of
+                    [] -> Left "Error!"
+                    [x] -> Right x
+                    _ -> Left "Err: Too many args!"
 
+-- = take 1 & drop 1
+pop :: [a] -> (a, [a])
+pop x = (head x, tail x)
+--push :: [a] -> a -> [a]
+--push = flip :
+push :: a -> [a] -> [a]
+push = (:)
+
+type Stack a = [a]
+emptyStack = []
+
+dispatch :: Stack Double -> String -> Stack Double
+dispatch (x:y:t) "+" =  (x+y):t
+dispatch (x:y:t) "-" =  (x-y):t
 
