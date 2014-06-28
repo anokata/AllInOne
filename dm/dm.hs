14aad35a612741ba209bc6117cd07ad952f8f949
anokata <quartenium@gmail.com>
2014-06-28 20:19:12 +0400

    некоторый стабильный момент

--- dm/dm.hs
+++ dm/dm.hs
@@ -1,12 +1,11 @@
 #!/usr/bin/env runhaskell
---Download manager via wget
---choose: vty(very nativ), vty-ui, hscurses(example bad work?)
+--Download manager via wget/ Menu Lib
 import qualified Graphics.Vty as VT
 import Graphics.Vty.Picture
 import Graphics.Vty.Attributes
-import Control.Concurrent --(threadDelay, forkIO)
-import Control.Monad(forever)
-import System.Exit
+--import Control.Concurrent --(threadDelay, forkIO)
+--import Control.Monad(forever)
+--import System.Exit
 import Data.Char
 
 data MenuState = MenuState {
@@ -39,7 +38,7 @@ type URL = String
 type BookmarkE = (BTitle, URL, ExecCmd)
 --type FolderE = (BTitle, Bookmarks, Opened)
 type Bookmarks = [BookmarkE]
-type InputMenuFunc = VT.Event -> MenuState -> MenuState
+type InputMenuFunc = VT.Event -> MenuState -> IO MenuState
 type RepaintMenuFunc = MenuState -> Image
 
 
@@ -67,9 +66,6 @@ showbookmarksSMenu b prefix = foldl showOneBookmarkMenu "" b
         showOneBookmarkMenu :: String -> BookmarkE -> String
         showOneBookmarkMenu acc  (t,_,_) = acc ++ prefix ++ t ++ "\n"
 
-
-discard :: Functor f => f a -> f ()
-discard = fmap (const ())
 
 blockFull = '\x2588'
 horBlock = replicate 20 blockFull
@@ -125,36 +121,43 @@ framedMenu l w attr =
         drawList = foldl 
           (\acc (s,a)->acc <-> lWalla <|> (string a s) <|> (translate (w - length s, 0) lWalla)) empty_image l
 
---handledAKeys = ['d','i','k']
-
 repaintOnEvent :: (VT.Event, VT.Vty) -> MenuState -> IO MenuState
 repaintOnEvent (e, v) m = 
     let 
     f = gFinput m 
-    newmenustate = (f e m)
     in do   
+        newmenustate <- f e m
         repaint (e, v) newmenustate
         return newmenustate
 
--- да, проще избавиться от этой вложенности. и будет просто список элементов. потом возможно и список каталогов и всё. отдельно.
+--data Opt a = Opt a | Nopt (IO a)
 
-handleInput :: VT.Event -> MenuState -> MenuState
+handleInput :: VT.Event -> MenuState -> IO MenuState
 handleInput e m@(MenuState i l b inpf rf _ _) = 
     case e of 
         (VT.EvKey (VT.KASCII k) []) -> 
             case k of
-                'i' | i>1 -> m {gCurrentMenuEl=(i-1)}
-                'i' -> m {gCurrentMenuEl=l}
-                'k' | i<l -> m{gCurrentMenuEl=(i+1)}
-                'k' -> m{gCurrentMenuEl=1}
-                'd' -> deleteCurrent m
-                'a' -> m {
-                            gBuffer = "e",
+                'i' | i>1 -> return m {gCurrentMenuEl=(i-1)}
+                'i' -> return m {gCurrentMenuEl=l}
+                'k' | i<l -> return m{gCurrentMenuEl=(i+1)}
+                'k' -> return m{gCurrentMenuEl=1}
+                'd' -> return $ deleteCurrent m
+                'a' -> do
+                    let 
+                        oldFinput = gFinput m
+                        oldFpaint = gFpaint m
+                    
+                    newmenu <- runInterfaceAndWaitFor m {
+                            gBuffer = "=",
                             gFinput = addInput,
                             gFpaint = addPaint
-                         }
-                otherwise -> m
-        otherwise -> m
+                         } (VT.EvKey VT.KEnter [])
+                    return newmenu{gFinput=oldFinput, gFpaint = oldFpaint,
+                        gLenMenu = 1+(gLenMenu newmenu),
+                        gBook = addBookmark (gBook newmenu) ("added title",gBuffer newmenu ,"cmd")
+                        }
+                otherwise -> return m
+        otherwise -> return m
 
 getSelectedUrl :: MenuState -> String
 getSelectedUrl (MenuState i l b _ _ _ _) = if l > 0 then
@@ -168,9 +171,6 @@ bookmarkAt b n = b !! (n-1)
 repaint :: (VT.Event, VT.Vty) -> MenuState -> IO ()
 repaint (e, v) m@(MenuState _ _ _ inputF paintF _ _) = 
     let 
-        --img = framedList ["(1.)","(2.) - ","(1) list node.","abraCaDabra","------",show e] 15 defAttr
-        --img = framedList (lines $ showbookmarks testbookmarks) 25 defAttr
-        --img = --<-> (string defAttr (show m ++(show e)))
         img = paintF m
         pic = Picture (Cursor 0 0) img (Background ' ' defAttr)
     in
@@ -186,39 +186,30 @@ deleteCurrent m@(MenuState i l b xf zf bf e) | l>1 = MenuState (if i==1 then i e
 deleteAt :: Int -> [a] -> [a]
 deleteAt n l = take (n-1) l ++ drop n l
 
--- тут мы должны спросить строку.. принципиально тут уже нужно вводить новое состояние, в котором и рис и обработка другая.. тогда может либо меню же и будет содержать функцию обработчик либо лишь перечисление из состояний? что лучше...
--- давай делать что первое приходит в голову
 addBookmark :: Bookmarks -> BookmarkE -> Bookmarks
 addBookmark b x = b ++ [x]
 
 -- функции рисования и обработки ввода для добавления записи
 addPaint :: MenuState -> Image
-addInput :: VT.Event -> MenuState -> MenuState
+addInput :: VT.Event -> MenuState -> IO MenuState
 
 addPaint m = (string defAttr "Enter new:") <-> framedList [gBuffer m] 30 defAttr 
 
 addInput e m = 
-    case e of 
-        (VT.EvKey VT.KEnter []) -> m{
-                           gBook = addBookmark (gBook m) ("added title",gBuffer m ,"cmd"),
-                           gFinput = handleInput,
-                           gFpaint = paintMenu,
-                           gLenMenu = 1+(gLenMenu m)
-                      }
+    return $ case e of 
         (VT.EvKey (VT.KASCII k) []) -> 
             case k of
                 '\b' -> m{gBuffer= tail (gBuffer m)}
                 otherwise -> m{gBuffer= (gBuffer m) ++ [k]}
         otherwise -> m
 
---(VT.EvKey (VT.KASCII 'q') [])
 runInterfaceAndWaitFor :: MenuState -> VT.Event -> IO MenuState
 runInterfaceAndWaitFor m exi = do
     let v = gVty $ gIDFIM m
+    repaintOnEvent (VT.EvResize 0 0, v) m
     event <- VT.next_event v
     case event of
         k | k==exi -> return m {gIDFIM = (gIDFIM m){ gState = Exit}}
-        --(VT.EvKey (VT.KASCII k) []) | k==exi -> return m {gIDFIM = (gIDFIM m){ gState = Exit}}
         otherwise -> do
             newmenu <- repaintOnEvent (event, v) m
             runInterfaceAndWaitFor newmenu exi
@@ -230,7 +221,7 @@ main = do
     --putStrLn (showbookmarks n)
     v <- VT.mkVty
     let 
-        m = MenuState 1 (length (lines $ showbookmarksMenu n)) n addInput addPaint "" (IDFIM {gVty = v, gState = Proceed})
+        m = MenuState 1 (length (lines $ showbookmarksMenu n)) n handleInput paintMenu "" (IDFIM {gVty = v, gState = Proceed})
 
     repaintOnEvent ((VT.EvResize 0 0), v) m
     res <- runInterfaceAndWaitFor m (VT.EvKey (VT.KASCII 'q') [])
