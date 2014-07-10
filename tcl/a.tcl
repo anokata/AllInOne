b97022e8be9813fdf36b302dd480dda2d75b2ca9
anokata <quartenium@gmail.com>
2014-07-10 23:32:43 +0400

    свой интерпретатор перезаписи строк на tcl

--- tcl/a.tcl
+++ tcl/a.tcl
@@ -33,8 +33,8 @@ set x abc
 switch $x ab "puts ab" default "puts no"
 if {$x=="abc"} {puts "==abc"} else {puts "noever"}
 
-proc sum {a b} {
-    return [expr $a + $b] }
+proc sum {a b {def 4}} {
+    return [expr 4*($a + $b)] }
 
 puts [sum 3 8]
 
@@ -63,7 +63,7 @@ button .ex -text Выход -command exit  -relief flat
 button .ex2 -textvariable date -command {pack .hi .ex .ex2 .ex$i -expand yes -fill both} -relief flat
 label .clock -textvar time  -foreground "#FFEECC" -background "#223344" -activebackground "#001122" -activeforeground "#001122"
 ttk::button .ex4 -text "Hello" -style "Fun.TButton" 
-pack .hi .ex .ex2 .ex4 .clock -expand yes -fill none
+pack .hi .ex .ex2 .ex4 .clock -expand yes -fill none -anchor w
 set i 3
 button .ex$i -text Title -command {wm title . $i} -relief flat
 
@@ -83,7 +83,7 @@ foreach {id day} { 1 Понедельник
  2 Вторник 3 Среда 4 Четверг
  5 Пятница 6 Суббота 7 Воскресенье
 } {
-	pack [label .l$id -text $day]
+	pack [label .l$id -text $day] -anchor w
 }
 
 set a ""
@@ -92,7 +92,7 @@ label .la -text {Маленькие буквы:}
 entry .a -bg white -textvariable a \
 	 -invcmd bell -validate all -vcmd {Letter %P}
 
-pack .la .a -fill x
+pack .la .a -fill x -anchor w
 
 proc Digit {text} {
 	if {[regexp {[^0-9]} $text]} {
@@ -108,10 +108,46 @@ label .ld -text {Цифры:}
 entry .d -bg white -textvariable d \
 	-invcmd bell -validate all -vcmd {Digit %P}
 
-pack .ld .d -fill x
+pack .ld .d -fill x -anchor w
 bind . <Key-q> {exit}
 
+#string map $list $input
 
+place [text .textbox] -x 200 -width 200 -height 250
+place [label .res -text "result:" -textvar result] -x 200 -y 250
+place [button .run -text "Run" -command runsubs] -x 200 -y 270
+wm resizable . 0 0
+wm geometry . "=500x400+0+0"
 
+proc runsubs {} {
+    set cont [.textbox dump -text "0.0" "end"]
+    .textbox dump -text -command textget "0.0" "end"
+    set cont2 [.textbox get "0.0" "end"]
+    set rules [string range $cont2 0 [string first "\n->\n" $cont2]]
+    set init  [string range $cont2 [expr 3+[string first "\n->\n" $cont2]] "end"]
+    set rules2 [lsearch -all -inline -not [string map {"->" " -> " } $rules] "->"]
+    set result [string map $rules2 $init]
+    puts IS:$result
+    set res2 [lsearch -all -inline -not $result "\n"]
+    puts :$res2
+    #puts [array exists cont]
+    #puts [lindex $cont 1]
+    #array set conta $cont
+    #puts [array exists conta]
+    #puts [array names conta]
+    #foreach i $cont {puts >>$i}
+    #set ::result "f"
+    set ::result $res2
+    return $res2
+}
 
+set l [list a b c]
+puts $l
+puts [lindex $l 1]
+
+.textbox insert "0.0" "(start)->nn \n(end)->kk\n->\n(start)J(end)"
+
+proc textget {k v i} {
+    puts =$v;
+}
 
