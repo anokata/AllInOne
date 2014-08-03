dd456d5153eebd6e116690adcb72b5d493d9ecf8
anokata <quartenium@gmail.com>
2014-08-03 17:51:23 +0400

    add text & handler.. work on strict editor

--- cs/cs3ui/cs3ui/gu.cs
+++ cs/cs3ui/cs3ui/gu.cs
@@ -1,75 +1,52 @@
-using System; 
-using System.ComponentModel; 
-using System.Windows.Forms; 
- 
-namespace My{
-class RosettaInteractionForm : Form
-{    
-    // Model used for DataBinding.
-    // Notifies bound controls about Value changes.
-    class NumberModel: INotifyPropertyChanged
-    {
- 
-        Random rnd = new Random();
- 
-        // initialize event with empty delegate to avoid checks on null
-        public event PropertyChangedEventHandler PropertyChanged = delegate {};
- 
-        int _value;
-        public int Value
-        {
-            get { return _value; }
-            set 
-            { 
-                _value = value;
-                // Notify bound control about value change
-                PropertyChanged(this, new PropertyChangedEventArgs("Value"));
-            }
-        }
- 
-        public void ResetToRandom(){
-            Value = rnd.Next(5000);
-        }
-    }
- 
-    NumberModel model = new NumberModel{ Value = 0};
- 
-    RosettaInteractionForm()    
-    {
-        //MaskedTextBox is a TextBox variety with built-in input validation
-        var tbNumber = new MaskedTextBox
-                        { 
-                            Mask="0000",            // allow 4 decimal digits only
-                            ResetOnSpace = false,   // don't enter spaces
-                            Dock = DockStyle.Top    // place at the top of form
-                        };
-        // bound TextBox.Text to NumberModel.Value;
-        tbNumber.DataBindings.Add("Text", model, "Value");
- 
-        var btIncrement = new Button{Text = "Increment", Dock = DockStyle.Bottom};
-        btIncrement.Click += delegate
-                        {
-                            model.Value++;
-                        };
-        var btDecrement = new Button{Text = "Decrement", Dock = DockStyle.Bottom};
-        btDecrement.Click += delegate
-                        {
-                            model.Value--;
-                        };
-        var btRandom = new Button{ Text="Reset to Random", Dock = DockStyle.Bottom };
-        btRandom.Click += delegate
-                        {
-                            if (MessageBox.Show("Are you sure?", "Are you sure?", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                                model.ResetToRandom();
-                        };
-        Controls.Add(tbNumber);
-        Controls.Add(btIncrement);
-        Controls.Add(btDecrement);
-        Controls.Add(btRandom);
-    }
-    static void Main()
-    {
-        Application.Run(new RosettaInteractionForm());
-    }
-}
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MWFTestApplication {
+	class MainWindow : System.Windows.Forms.Form {	
+		static Label label;
+		static TextBox text;
+		RichTextBox text2;
+		public MainWindow() {
+
+			ClientSize = new System.Drawing.Size (450, 450);
+
+			label = new Label();
+			label.Text = "A: ";
+			label.Dock = DockStyle.Bottom;
+			label.TextAlign = ContentAlignment.MiddleCenter;
+			this.Controls.Add(label);
+			Text = "E/";
+			text = new TextBox ();
+			text.Dock = DockStyle.Fill;
+			text.Size = new System.Drawing.Size(200, 200);
+			text.Multiline = true;
+			text.Font = new System.Drawing.Font("Monospace", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			text.Text = "some";
+			text.Location = new System.Drawing.Point(10, 10);
+			text.Enabled = true;
+			text.AutoCompleteMode = AutoCompleteMode.Suggest;
+			text.KeyDown += keydownText;
+			text.KeyPress += keypressText;
+			this.Controls.Add (text);
+
+		}
+
+		static void keydownText (object s, KeyEventArgs e){
+			MainWindow.label.Text = e.KeyValue.ToString();
+			//e.SuppressKeyPress
+			e.Handled = true;
+		}
+		static void keypressText (object s, KeyPressEventArgs e){
+			MainWindow.label.Text += e.KeyChar;
+			//e.SuppressKeyPress
+			e.Handled = true;
+		}
+
+		public static void Main(string[] args) {
+			Application.Run(new MainWindow());
+		}
+	}
 }
--- cs/test1/test1/Program.cs
+++ cs/test1/test1/Program.cs
@@ -1,5 +1,6 @@
 using System;
 
+
 namespace test1
 {
 	class MainClass
