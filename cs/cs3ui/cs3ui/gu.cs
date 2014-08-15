0b116753d754b67c674573be19f50aa8075dca02
anokata <quartenium@gmail.com>
2014-08-15 22:19:24 +0400

    hakell hxt xml start разборки

--- cs/cs3ui/cs3ui/gu.cs
+++ cs/cs3ui/cs3ui/gu.cs
@@ -3,7 +3,12 @@ using System.IO;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
-
+/*=======TODO============================
+ * 1. SyntaxColor
+ * 1.1 поменять у части текста цвет\стиль
+ * 2. рог на datagrid?
+ *
+/*==============================================*/
 namespace MWFTestApplication {
 	class MainWindow : System.Windows.Forms.Form {	
 		static Label label;
@@ -11,6 +16,8 @@ namespace MWFTestApplication {
 		RichTextBox text2;
 		enum ModKeys {Nok, Ctrl, Shift};
 		static ModKeys mkeys = ModKeys.Nok;
+		static bool ctrl = false;
+		static char k;
 
 		public MainWindow() {
 
@@ -18,22 +25,42 @@ namespace MWFTestApplication {
 
 			label = new Label();
 			label.Text = "A: ";
-			label.Dock = DockStyle.Bottom;
+			label.Dock = DockStyle.Top;
 			label.TextAlign = ContentAlignment.MiddleCenter;
 			this.Controls.Add(label);
 			Text = "E/";
 			text = new TextBox ();
-			text.Dock = DockStyle.Fill;
+			text.Dock = DockStyle.Left;
 			text.Size = new System.Drawing.Size(200, 200);
 			text.Multiline = true;
 			text.Font = new System.Drawing.Font("Monospace", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 			text.Text = "some";
-			text.Location = new System.Drawing.Point(10, 10);
+			//text.Location = new System.Drawing.Point(10, 10);
 			text.Enabled = true;
 			text.AutoCompleteMode = AutoCompleteMode.Suggest;
 			text.KeyDown += keydownText;
 			text.KeyPress += keypressText;
 			this.Controls.Add (text);
+
+			text2 = new RichTextBox ();
+			text2.Dock = DockStyle.Right;
+			text2.Multiline = true;
+			text2.Font = new System.Drawing.Font("Monospace", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.Controls.Add (text2);
+			text2.Text = "asdf asdf afs asf ";
+			text2.Select (2, 3);
+			text2.Font = new Font("Consolas", 18f, FontStyle.Bold);
+			text2.BackColor = Color.AliceBlue;
+			Color[] colors =
+			{
+				Color.Aqua,
+				Color.CadetBlue,
+				Color.Cornsilk,
+				Color.Gold,
+				Color.HotPink,
+				Color.Lavender,
+				Color.Moccasin
+			};
 
 		}
 
@@ -43,15 +70,20 @@ namespace MWFTestApplication {
 				MainWindow.mkeys = ModKeys.Ctrl;
 			else
 				MainWindow.mkeys = ModKeys.Nok;
+			ctrl = e.Shift;
+
 			//e.SuppressKeyPress
-			e.Handled = true;
+			//e.Handled = true;
 		}
 		static void keypressText (object s, KeyPressEventArgs e){
 			MainWindow.label.Text += e.KeyChar;
 			if (MainWindow.mkeys != ModKeys.Ctrl)
 				MainWindow.label.Text += "C";
+
 			//e.SuppressKeyPress
+			k = e.KeyChar;
 			e.Handled = MainWindow.mkeys != ModKeys.Ctrl;
+			e.Handled = ! ctrl;
 
 		}
 
---
+++ haskell/xml.hs
@@ -0,0 +1,10 @@
+import Data.Tree.Class
+import Data.Tree.NTree.TypeDefs
+import qualified Text.XML.HXT.DOM.FormatXmlTree as F
+import qualified Text.XML.HXT.Parser.XmlParsec as P
+
+testXML = "<>"
+
+main = do 
+    
+    print "end."
