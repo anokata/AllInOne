cf2f209ea737e8b237df5e61231759a3a67124b0
anokata <quartenium@gmail.com>
2014-08-16 13:12:36 +0400

    сделал мап замену в дереве всём

--- haskell/xml.hs
+++ haskell/xml.hs
@@ -8,15 +8,16 @@ import qualified Text.XML.HXT.DOM.XmlNode as XN
 import Data.Monoid
 --import qualified Text.XML.HXT.DOM.XmlTreeFilter as R
 {-TODO
-- [ ] Функцию заменяющую тэг с атрибутом с *данным* именем, с текстом на *данный* текст
+- [x] Функцию заменяющую тэг с атрибутом с *данным* именем, с текстом на *данный* текст
         тип :: XNode -> String -> String -> XNode
         арг node attrName newText
             - [x] сделаем тестовые данные, заменим текст тэга с атрибутом target 
                 "<not notarget='f'>text</not>"
                 "<tot target='s'>source</tot>" -> "<tot target='s'>dest</tot>"
         - [x] функцию проверки тэга :: attrnode tagName tagText :: XNode -> String -> String -> Bool
-- [ ] промапить данной функцией всё дерево
-- [ ] Функцию заменяющую тэг с атрибутом с *данным* именем и *данным* значением, с текстом на *данный* текст
+- [x] Функцию заменяющую тэг с атрибутом с *данным* именем и *данным* значением, с текстом на *данный* текст
+- [?] промапить данной функцией всё дерево, вглубь
+    - [ ] надо лучше протестировать
 -}
 isThatAttr :: NTree H.XNode -> String -> String -> Bool
 isThatAttr (NTree (H.XAttr qname) [NTree (H.XText text) []]) tagName tagText | (QN.localPart qname) == tagName && text == tagText = True
@@ -35,19 +36,19 @@ testrepltext2 = fromMaybe $ XN.getText $ head  $ Tree.getChildren $ head test1x
 
 test2 = P.xread "<tot otherattr='da' target='s'>source</tot>"
 test2AttrsIs = or $ (fmap (\x-> isThatAttr x "target" "s") (getAttrf $ head test2))
-test3 = replaceTextIn (head test1x) "NEWTEXT"
+test3 = replaceTextIn (head test1x) "NEWTEXT" "target" "s"
 test3n = P.xread "<tot otherattr='da' targets='s'>source</tot>"
 test3a = P.xread "<tot otherattr='da' target='a'>source</tot>"
-test3nn = replaceTextIn (head test3n) "NEWTEXT"
-test3aa = replaceTextIn (head test3a) "NEWTEXT"
+test3nn = replaceTextIn (head test3n) "NEWTEXT" "targets" "s"
+test3aa = replaceTextIn (head test3a) "NEWTEXT" "target" "s"
 
 
-replaceTextIn :: NTree H.XNode -> String -> NTree H.XNode
-replaceTextIn n@(NTree (H.XTag tagName attrList) childWithText) newText = 
-    if (or (fmap (\x-> isThatAttr x "target" "s") attrList)) then 
+replaceTextIn :: NTree H.XNode -> String -> String -> String -> NTree H.XNode
+replaceTextIn n@(NTree (H.XTag tagName attrList) childWithText) newText attrName attrVal = 
+    if (or (fmap (\x-> isThatAttr x attrName attrVal) attrList)) then 
         (NTree (H.XTag tagName attrList) (replaceText childWithText newText))
-        else n
-replaceTextIn n _ = n
+        else (NTree (H.XTag tagName attrList) (fmap (\x-> replaceTextIn x newText attrName attrVal) childWithText))  -- n
+replaceTextIn n _ _ _ = n
 
 replaceText :: H.XmlTrees -> String -> H.XmlTrees
 replaceText ((NTree (H.XText _) [])  : other) newText = ((NTree (H.XText newText) [])  : other)
@@ -69,21 +70,25 @@ fromMaybe a = maybe mempty id a
       [NTree (XText "source") []] -}
 --то есть надо у тэга проверить атрибут и если он подходит то найти текст и если он есть заменить его
 
-testXML = "<a><A y='Y'>tx</A></a><b z='Z'></b><c x='X' xx='_'></c>"
-a = P.xread testXML
-b = F.formatXmlTree $ head a
+testXMLs = "<a><A y='Y'>tx</A></a><b z='Z'>ss</b><c x='X' xx='_'>dd</c>"
+testXML = P.xread testXMLs
+b = F.formatXmlTree $ head testXML
 c = putStrLn b
 
-d = fmap F.formatXmlTree a
+d = fmap F.formatXmlTree testXML
 e = mapM putStrLn d
 showx x = mapM putStrLn (fmap F.formatXmlTree x)
 
-f = head a
+f = head testXML
 --g H.XTag x = x
 g (H.XText x) = H.XText $ "new" ++ x
 g x = x 
 h = fmap g f
 i = showx [h]
+-- ============
+ftest = fmap (\x-> replaceTextIn x "" "" "" ) testXML
+ftesta a b c = fmap (\x-> replaceTextIn x a b c ) testXML
+ptest a b c = showx (ftesta a b c)
 
 main = do 
     
