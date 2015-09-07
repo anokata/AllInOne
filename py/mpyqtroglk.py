4fee2dab654d358d7b528fad7d8b90e3fc2c6944
anokata <quartenium@gmail.com>
2015-09-07 15:30:43 +0300

    Update mpyqtroglk.py

--- py/mpyqtroglk.py
+++ py/mpyqtroglk.py
@@ -3,22 +3,22 @@ import sys, random
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor, QFont, QPen
 from PyQt5.QtCore import Qt, QRect
-
+# функция создания двухмерного массива(списка списков) с начальным значением
 def makeArray2D(w, h, initVal):
     return [[initVal for x in range(w)] for x in range(h)]
-#У нас есть тайлы
+#У нас есть тайлы. Тайл - символ с цветом символа и цветом фона
 class Tile(object):
     symbol = ' '
     fcolor = QColor(0, 0, 0)
     bcolor = QColor(0, 0, 0, 0)
-#из тайлов состоит поле прямоугольное
+#из тайлов состоит поле прямоугольное. Поле - массив тайлов, параметры поля.
 class Field(object):
     w = 10
     h = 10
     tileWidth = 20
     tileHeight = 20
     tiles = []
-    
+    # создание изначального поля
     def __init__(self, w = 10,h = 10):
         self.w = w
         self.h = h
@@ -29,7 +29,7 @@ class Field(object):
         
 #просто главный класс-окно-приложение
 class Example(QWidget):
-    
+    # настройка окна
     def __init__(self):
         super().__init__()
         self.setGeometry(100, 100, 580, 470)
@@ -38,36 +38,38 @@ class Example(QWidget):
     
     # а тут рисуем
     def paintEvent(self, event):
+        # инициализация
         qp = QPainter()
         qp.begin(self)
+        # тесты
         qp.setPen(QColor(1, 100, 200))
         qp.setFont(QFont('Lucida Console', 14))
         #qp.drawText(event.rect(), Qt.AlignLeft, "Score: ")   
         #qp.drawText(10,100, "Score: \nSecond line\n#..T..#\n##..#.#\n#TTT..#")   
-        
         qp.setPen(Qt.red)
         #size = self.size()
         qp.drawPoint(2, 2)
-
         qp.setBrush(QColor(200, 0, 0))
         qp.drawRect(10, 15, 90, 60)
-        
         pen = QPen(Qt.black, 2, Qt.SolidLine)
-
         qp.setPen(pen)
         qp.drawLine(20, 40, 250, 40)
         
-        #drawTiles of field
+        #drawTiles of field. Рисуем тайлы - Вынести в функции(какого класса?)
         for x in range(field.w):
             for y in range(field.h):
                 qp.setPen(field.tiles[x][y].fcolor)
                 qp.drawText(event.rect(), Qt.AlignCenter, field.tiles[x][y].symbol) 
-                
+        
+        #тест многострочного текста        
         qp.setBrush(QColor(0, 0, 0))
         qp.setPen(QColor(100,0,0))
         qp.drawText(QRect(0,0,200,200), Qt.AlignLeft, "Score: \nSecond line\n#..T..#\n##..#.#\n#TTT..#")   
+        
+        # финализация
         qp.end()
         
+    # функция рисования тайла
     def drawTile(t):
         pass
      
