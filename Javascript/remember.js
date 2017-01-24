10cddc44c2bb407385f9136acf06d2cebc27731f
anokata <quartenium@gmail.com>
2017-01-24 21:02:28 +0300

    Start to remember JS. Сделаю стандартные свои задачи и буду писать конспектик.

---
+++ Javascript/remember.js
@@ -0,0 +1,11 @@
+//alert console.log
+'use strict'
+var a = 5; // num
+a = a * 2 + 3 % 2;
+var b = 'string';
+// true null undefined NaN Infinity
+// fun
+const some_fun = (arg) => {
+    return arg*2;
+}
+console.log(some_fun(2));
