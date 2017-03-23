afd7ee9350c0e74acc6a210887e2f3adb11c4ef8
anokata <quartenium@gmail.com>
2017-03-23 18:40:57 +0300

    php pdo

--- php/hello.php
+++ php/hello.php
@@ -1,7 +1,15 @@
 <?php
-print("hello cruel world :( Hi $argv[1]");
-//comment
-/*
-#dfjl
-    #*/
+print("hello cruel world :( Hi $argv[1]\n");
+//phpinfo();
+$pdo = new PDO('mysql:host=localhost;dbname=mor', 'test', 'test');
+var_dump($pdo);
+$sth = $pdo->query('SELECT * FROM territory');
+
+foreach ($sth as $row) {
+        print $row['id'] . "\t";
+        print $row['name'] . "\n";
+    }
+
+$sth = null;
+$pdo = null;
 ?>
