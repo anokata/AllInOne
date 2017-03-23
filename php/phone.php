830c7c0da74b619735c1d5aa5fa5e8f1c7637148
anokata <quartenium@gmail.com>
2017-03-23 23:40:11 +0300

    finished view query phone book

--- php/phone.php
+++ php/phone.php
@@ -6,11 +6,28 @@
 <body>
 <table>
 <thead>
-<tr><th>Name</th></tr>
+<tr><th>Name</th><th>Phone</th></tr>
 </thead>
 <?php
 // TODO view peop, search by num, add, del.
+$connection = new PDO('mysql:host=localhost;dbname=phonebook', 'test', 'test');
+if (!$connection) {
+    exit(1);
+}
 
+$query = $connection->query(
+    'select name, phone from people inner join phones where people.id = phones.people_id');
+
+foreach ($query as $row) {
+        ?> <tr><td> <?php
+        print $row['name'];
+        ?></td><td><?php
+        print $row['phone'];
+        ?> </td></tr> <?php
+    }
+
+$query = null;
+$connection = null;
 ?>
 </table>
 
