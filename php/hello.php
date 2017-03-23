ee30d2b9ab7fd7c0de3a83010f724792b0893c41
anokata <quartenium@gmail.com>
2017-03-23 22:59:04 +0300

    add phone. chg to learn

--- php/hello.php
+++ php/hello.php
@@ -1,15 +1,22 @@
+<html>
+<body>
 <?php
 print("hello cruel world :( Hi $argv[1]\n");
 //phpinfo();
 $pdo = new PDO('mysql:host=localhost;dbname=mor', 'test', 'test');
 var_dump($pdo);
+print(serialize($argv));
 $sth = $pdo->query('SELECT * FROM territory');
 
 foreach ($sth as $row) {
-        print $row['id'] . "\t";
-        print $row['name'] . "\n";
+        ?> <div> <?php
+        print $row['id'] . " ";
+        print $row['name'] . "";
+        ?> </div> <?php
     }
 
 $sth = null;
 $pdo = null;
 ?>
+</body>
+</html>
