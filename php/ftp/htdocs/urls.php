e6d08c42eac408e75e91dcd352f6516745805c38
anokata <quartenium@gmail.com>
2017-03-25 20:11:43 +0300

    eu.ai

--- php/ftp/htdocs/urls.php
+++ php/ftp/htdocs/urls.php
@@ -1,10 +1,9 @@
 <?php
 require('db.php');
 require('forms.php');
-require('settings.php');
 make_head("URLS", 'style.css');
 print("before connect<br>");
-$connection = db_connect('mysql', 'sql202.eu.ai', 'euai_19877563_urls', 'euai_19877563', $db_password);
+$connection = db_connect('mysql', 'sql202.eu.ai', 'euai_19877563_urls', 'euai_19877563', 'wa8va128');
 print("after connect<br>");
 $name = isset($_POST['name']) ? $_POST['name'] : NULL;
 $method = isset($_POST['method']) ? $_POST['method'] : NULL;
