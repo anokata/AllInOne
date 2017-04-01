c5e0a1e0fffc6d44014fb947feed769106e7b347
anokata <quartenium@gmail.com>
2017-04-01 10:11:23 +0300

    mvc

---
+++ php/mvcapp1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+header {
+    background-color: #fee;
+}
+footer {
+    background-color: #eef;
+    position: relative;
+}
+</style>
+</head>
+<body>
+<?php require('templates/header.php'); ?>
+<?php require('router.php'); ?>
+<?php require('templates/footer.php'); ?>
+</body>
+</html>
