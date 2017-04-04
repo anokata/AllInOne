f25b8be79c863b0890a97ac451923086471ff035
anokata <quartenium@gmail.com>
2017-04-04 13:51:07 +0300

    package hello

--- java/ai/eu/work/train0/HelloWorld.java
+++ java/ai/eu/work/train0/HelloWorld.java
@@ -1,7 +1,9 @@
 package ai.eu.work.train0;
+import ai.eu.work.train0.SubTest;
 
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello virtual world!");
+        System.out.println(SubTest.test());
     }
 }
---
+++ java/ai/eu/work/train0/SubTest_.java
@@ -0,0 +1,7 @@
+package ai.eu.work.train0;
+
+class SubTest {
+    public static String test() {
+        return "hi from sub test not ";
+    }
+}
