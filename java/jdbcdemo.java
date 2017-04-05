a914235e9491b400532a693bed387eafbe361eef
anokata <quartenium@gmail.com>
2017-04-05 22:45:56 +0300

    jdbc test get select result from connection to postresql db

--- java/jdbcdemo.java
+++ java/jdbcdemo.java
@@ -3,19 +3,24 @@ import java.util.Enumeration;
 import java.sql.Driver;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 class jdbcdemo {
     public static void main(String[] args) throws SQLException {
-        Enumeration<Driver> ds = DriverManager.getDrivers();
-        while (ds.hasMoreElements()) {
-            System.out.println("Driver: ");
-        }
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db");
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from test");
+        String url = "jdbc:postgresql://localhost/phonebook?user=test&password=test&ssl=false";
+        Connection conn = DriverManager.getConnection(url);
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet rs = stmt.executeQuery("select * from phones");
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int numberOfColumns = rsmd.getColumnCount();
         rs.first();
         while (rs.next()) {
-            
+           for (int i = 1; i <= numberOfColumns; i++) {
+               System.out.print(rs.getObject(i) + " | "); 
+           }
+           System.out.println();
         }
         rs.close();
         stmt.close();
