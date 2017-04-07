b8f1159b46e2d495660a36cd05b8e9528e4c6d03
anokata <quartenium@gmail.com>
2017-04-07 16:28:07 +0300

    move old js

---
+++ old/js/console/src/screen.js
@@ -0,0 +1,13 @@
+log = console.log;
+createElement = document.createElement;
+
+function createScreen() {
+    let div = createElement('div');
+}
+
+function main() {
+    log('main');
+    createElement();
+}
+
+main();
