cd5961eae707e212e6dc2fd5db9f3b048aaaa147
anokata <quartenium@gmail.com>
2017-04-08 09:27:01 +0300

    first servlet hello world

---
+++ java/tomcat_project_one/src/hello/HelloWorld.java
@@ -0,0 +1,27 @@
+package hello;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class HelloWorld extends HttpServlet {
+ 
+  private String message;
+
+  public void init() throws ServletException {
+      message = "In World";
+  }
+
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException
+  {
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + message + "</h1>");
+  }
+  
+  public void destroy() { }
+}
--- java/tomcat_project_one/src/hello/hello.java
+++ java/tomcat_project_one/src/hello/hello.java
@@ -1,6 +1,25 @@
 package hello;
 
-class hello {
-    public static void main(String[] args) {
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+
+public class hello extends HttpServlet {
+
+    public void init() throws ServletException { }
+    public void destroy() { }
+
+    public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("Hi from code");
+        out.println("</body></html>");
+        out.close();
+        throw new IOException("hi");
     }
 }
