e7e4359d2bf1032dd92b9a3f04eb3020a5ff827c
anokata <quartenium@gmail.com>
2017-04-08 09:40:48 +0300

    jdbc servlet init

--- java/tomcat_project_one/src/hello/hello.java
+++ java/tomcat_project_one/src/hello/hello.java
@@ -4,6 +4,14 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 
+import java.sql.DriverManager;
+import java.util.Enumeration;
+import java.sql.Driver;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 public class hello extends HttpServlet {
 
@@ -18,8 +26,40 @@ public class hello extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("Hi from code");
+        doDB(out);
+
         out.println("</body></html>");
         out.close();
         throw new IOException("hi");
+    }
+
+    private void doDB(PrintWriter out) {
+        try {
+            // TODO connect and load in init
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                out.println("can not found pgsql driver class");
+            }
+            String url = "jdbc:postgresql://localhost/phonebook?user=test&password=test&ssl=false";
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = stmt.executeQuery("select * from phones");
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int numberOfColumns = rsmd.getColumnCount();
+            rs.first();
+            while (rs.next()) {
+               for (int i = 1; i <= numberOfColumns; i++) {
+                   out.println(rs.getObject(i));
+                   out.println(" | ");
+               }
+               out.println("<br>");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            out.print("SQLException" + e.getMessage());
+        }
     }
 }
