ac03d328b63867b0186cfb0fb08b7bb21890a756
anokata <quartenium@gmail.com>
2017-04-12 22:56:46 +0300

    develop weather getter and displayer

--- java/UrlGetter.java
+++ java/UrlGetter.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -24,13 +25,18 @@ class UrlGetter {
         return yc.getInputStream();
     }
 
-    public static Node getOneNodeForXPath(Document doc, String query)
+    public static Node getOneNodeForXPath(Object doc, String query)
+            throws XPathExpressionException {
+        return getNodesForXPath(doc, query).item(0);
+    }
+    
+    public static NodeList getNodesForXPath(Object doc, String query)
             throws XPathExpressionException {
         XPathFactory pathFactory = XPathFactory.newInstance();
         XPath xpath = pathFactory.newXPath();
         XPathExpression expr = xpath.compile(query);
         NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-        return nodes.item(0);
+        return nodes;
     }
 
     public static void main(String[] args) 
@@ -41,6 +47,7 @@ class UrlGetter {
         Node node = getOneNodeForXPath(doc, "//time[1]/windDirection");
         System.out.println("" + node.getAttributes().getNamedItem("deg"));
         System.out.println("current pressure: " + getCurrentPressure(doc));
+        System.out.println(getForecast(doc));
     }
 
     public static String getFirstWeatherParam(Document doc, String tag, String param)
@@ -59,5 +66,35 @@ class UrlGetter {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         return builder.parse(is);
+    }
+
+    //TODO: make method current weather in words 
+    //and how it change in near time(up or down
+    //temp, rain? wind raise?)
+    public static List<HashMap<String, String>> getForecast(Document doc)
+            throws XPathExpressionException {
+        //TODO: extract
+        //TODO: make get one and use it for now weather and this loop simplification
+        HashMap<String, String> forecastDict = new HashMap<String, String>();
+        forecastDict.put("windSpeed", "mps");
+        forecastDict.put("temperature", "value");
+        forecastDict.put("pressure", "value");
+
+        List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+        NodeList nodes = getNodesForXPath(doc, "//time"); // FIXME sorted?
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            HashMap<String, String> data = new HashMap<>();
+            for(Map.Entry<String, String> e : forecastDict.entrySet()) {
+                String key = e.getKey();
+                String attribute = e.getValue();
+                String value = getOneNodeForXPath(node, key)
+                    .getAttributes().getNamedItem(attribute)
+                    .getNodeValue();
+                data.put(key, value);
+            }
+            list.add(data);
+        }
+        return list;
     }
 }
