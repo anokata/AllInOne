bd5b7cc9e80439b06bbb5a7707a566ec7ff218e3
anokata <quartenium@gmail.com>
2017-04-29 22:21:52 +0300

    try gui..

---
+++ D/xox.d
@@ -0,0 +1,13 @@
+ import gtk.MainWindow;
+ import gtk.Label;
+ import gtk.Main;
+ 
+ void main(string[] args)
+ {
+     Main.init(args);
+     MainWindow win = new MainWindow("Hello World");
+     win.setDefaultSize(200, 100);
+     win.add(new Label("Hello World"));
+     win.showAll();
+     Main.run();
+ }
---
+++ D/xoxcli.d
@@ -0,0 +1,12 @@
+import std.stdio;
+
+struct game_state {
+    //field;
+}
+
+void main() {
+    writeln("XOX (tic-tac-toe) game. console variant. V0.1");
+
+
+    writeln("End.");
+}
