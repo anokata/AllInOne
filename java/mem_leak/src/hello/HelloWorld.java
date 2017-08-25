cfe8d1f60a757a1ffca19a95b20ad352a4a05962
anokata <anokata8@mail.ru>
2017-08-25 15:52:26 +0300

    (JAVA) tomcat reload, and mem using

--- java/mem_leak/src/hello/HelloWorld.java
+++ java/mem_leak/src/hello/HelloWorld.java
@@ -3,13 +3,16 @@ package hello;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.util.ArrayList;
 
 public class HelloWorld extends HttpServlet {
  
   private String message;
+  //static final int blob[] = new int[1024 * 1024 * 1024 * 100];
+  static final ArrayList list = new ArrayList(1024 * 1024);
 
   public void init() throws ServletException {
-      message = "In World";
+      message = "mem leak.";
   }
 
   public void doGet(HttpServletRequest request,
@@ -19,7 +22,20 @@ public class HelloWorld extends HttpServlet {
       response.setContentType("text/html");
 
       PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
+      out.println("<h4>" + message + "</h4>");
+      int blob[] = new int[1024 * 1024 * 1024 * 100];
+      ArrayList list = new ArrayList(1024 * 1024 * 1024);
+      for (int i = 0; i < 10; i++) {
+          try {
+              Thread.sleep(5000);
+          } 
+          catch(InterruptedException ex) {
+              Thread.currentThread().interrupt();
+          }
+          blob = new int[1024 * 1024 * 1024 * 100];
+          list = new ArrayList(1024 * 1024 * 1024);
+          out.println("<br> sleep Allocated 1");
+      }
   }
   
   public void destroy() { }
