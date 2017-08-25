8768e01cbd97ca3a0b8ee6e843474dc887f919d3
anokata <anokata8@mail.ru>
2017-08-25 14:43:03 +0300

    (JAVA) making min app for tomcat

---
+++ java/mem_leak/src/hello/HelloWorld.java
@@ -0,0 +1,26 @@
+package hello;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+ 
+  private String message;
+
+  public void init() throws ServletException {
+      message = "In World";
+  }
+
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException
+  {
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + message + "</h1>");
+  }
+  
+  public void destroy() { }
+}
