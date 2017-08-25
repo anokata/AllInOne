f116ce7f8358f25e68710761200736b696a6e6ab
anokata <anokata8@mail.ru>
2017-08-25 18:03:45 +0300

    (Java) mem use

--- java/mem_leak/src/hello/HelloWorld.java
+++ java/mem_leak/src/hello/HelloWorld.java
@@ -9,10 +9,10 @@ public class HelloWorld extends HttpServlet {
  
   private String message;
   //static final int blob[] = new int[1024 * 1024 * 1024 * 100];
-  static final ArrayList list = new ArrayList(1024 * 1024);
+  //static final ArrayList list = new ArrayList(1024 * 1024);
 
   public void init() throws ServletException {
-      message = "mem leak.";
+      message = "5mem leak.";
   }
 
   public void doGet(HttpServletRequest request,
@@ -23,19 +23,15 @@ public class HelloWorld extends HttpServlet {
 
       PrintWriter out = response.getWriter();
       out.println("<h4>" + message + "</h4>");
-      int blob[] = new int[1024 * 1024 * 1024 * 100];
-      ArrayList list = new ArrayList(1024 * 1024 * 1024);
-      for (int i = 0; i < 10; i++) {
-          try {
-              Thread.sleep(5000);
-          } 
-          catch(InterruptedException ex) {
-              Thread.currentThread().interrupt();
-          }
-          blob = new int[1024 * 1024 * 1024 * 100];
-          list = new ArrayList(1024 * 1024 * 1024);
-          out.println("<br> sleep Allocated 1");
+      out.println("<h4>" + message + "</h4>");
+      int blob[] = new int[1024 * 1024 * 1024 * 1000];
+      ArrayList list = new ArrayList(102004);
+      for (int i = 0; i < 10000; i++) {
+		  Object[] objects = new Object[10000000];
+          blob[i*2] = blob[i] + 1023;
       }
+      out.println("<br> sleep Allocated 2" + Integer.toString(blob[1024]));
+      out.println("<h4>" + message + "</h4>");
   }
   
   public void destroy() { }
