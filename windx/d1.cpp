99f822b620fc23cf37e5b3ee79ac350aade1b947
anokata <anokata8@mail.ru>
2017-10-29 16:13:41 +0300

    windx

--- windx/d1.cpp
+++ windx/d1.cpp
@@ -2,6 +2,16 @@
 #include <windows.h>
 #include <windowsx.h>
 #include <d3d9.h>
+#include <d3dx9shape.h>
+/*
+ *http://www.cplusplus.com/forum/windows/108166/
+http://www.intuit.ru/studies/courses/1120/175/lecture/4756?page=1
+ * sphere
+   texture
+   animation rotate
+   input move
+   sattelite
+*/
 
 /* Подключение библиотеки Direct3D */
 #pragma comment (lib, "d3d9.lib")
@@ -23,8 +33,7 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 int WINAPI WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine,
-                   int nCmdShow)
-{
+                   int nCmdShow) {
     HWND hWnd;
     WNDCLASSEX wc;
 
@@ -40,30 +49,20 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
     RegisterClassEx(&wc);
 
-    hWnd = CreateWindowEx(0,
-                          "WindowClass",
-                          "Our First Direct3D Program",
+    hWnd = CreateWindowEx(0, "WindowClass", "Our First Direct3D Program",
                           WS_OVERLAPPEDWINDOW,
                           300, 300,
                           800, 600,
-                          NULL,
-                          NULL,
-                          hInstance,
-                          NULL);
+                          NULL, NULL, hInstance, NULL);
 
     ShowWindow(hWnd, nCmdShow);
 
-    // set up and initialize Direct3D
     initD3D(hWnd);
 
-    // enter the main loop:
-
     MSG msg;
-
-    while(TRUE)
-    {
-        while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-        {
+    // enter the main loop:
+    while(TRUE) {
+        while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
@@ -76,18 +75,14 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
     // clean up DirectX and COM
     cleanD3D();
-
     return msg.wParam;
 }
 
 
-// this is the main message handler for the program
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch(message)
-    {
-        case WM_DESTROY:
-            {
+/* Главный обработчик сообщений */
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch(message) {
+        case WM_DESTROY: {
                 PostQuitMessage(0);
                 return 0;
             } break;
@@ -97,9 +92,8 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 }
 
 
-// this function initializes and prepares Direct3D for use
-void initD3D(HWND hWnd)
-{
+/* this function initializes and prepares Direct3D for use */
+void initD3D(HWND hWnd) {
     d3d = Direct3DCreate9(D3D_SDK_VERSION);    // create the Direct3D interface
 
     D3DPRESENT_PARAMETERS d3dpp;    // create a struct to hold various device information
@@ -109,6 +103,9 @@ void initD3D(HWND hWnd)
     d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // discard old frames
     d3dpp.hDeviceWindow = hWnd;    // set the window to be used by Direct3D
 
+    d3dpp.EnableAutoDepthStencil = TRUE;
+    d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+
 
     // create a device class using this information and the info from the d3dpp stuct
     d3d->CreateDevice(D3DADAPTER_DEFAULT,
@@ -117,28 +114,32 @@ void initD3D(HWND hWnd)
                       D3DCREATE_SOFTWARE_VERTEXPROCESSING,
                       &d3dpp,
                       &d3ddev);
+
+    d3ddev->SetRenderState(D3DRS_ZENABLE, TRUE);    // turn on the z-buffer
 }
 
 
-// this is the function used to render a single frame
-void render_frame(void)
-{
+/* this is the function used to render a single frame */
+void render_frame(void) {
     // clear the window to a deep blue
     d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
+    d3ddev->Clear(0, NULL, D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
 
     d3ddev->BeginScene();    // begins the 3D scene
-
     // do 3D rendering on the back buffer here
+    // 	
+
+    LPD3DXMESH sphere;
+    D3DXCreateSphere(d3ddev, 1.0f, 16, 16, &sphere, NULL);
+    sphere->DrawSubset(0);
 
     d3ddev->EndScene();    // ends the 3D scene
-
     d3ddev->Present(NULL, NULL, NULL, NULL);   // displays the created frame on the screen
 }
 
 
-// this is the function that cleans up Direct3D and COM
-void cleanD3D(void)
-{
-    d3ddev->Release();    // close and release the 3D device
-    d3d->Release();    // close and release Direct3D
+/* Функция освобождения ресурсов Direct3D и COM */
+void cleanD3D(void) {
+    d3ddev->Release();    // Закрыть 3D устройство
+    d3d->Release();    // Закрыть Direct3D
 }
