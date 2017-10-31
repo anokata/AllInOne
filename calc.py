aa347a27c3511561ef0061b6731a8a4cc52170da
tikhomirovsvl <tikhomirovsvl@mosreg.ru>
2017-10-31 09:02:00 +0300

    [py] test math task

---
+++ calc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+def a():
+    x = (7-6.35)/6.5 + 9.9
+    y = (1.2/36 + 1.2/0.25) - (1 + 5/16.0)
+    z = y/(169/24.0)
+    u = x/z
+    print(x, y, z, u)
+    return u
+
+a()
