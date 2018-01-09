0b05ff25364f174c08ec0482a226fcbb4d402e0e
anokata <anokata8@mail.ru>
2018-01-09 21:29:12 +0300

    d3d

--- windx/d1.cpp
+++ windx/d1.cpp
@@ -1,4 +1,4 @@
-/* Подключение заголовочных файлов с описанимями базовых функций windows и Direct3D */
+/* Подключение заголовочных файлов с описаниями базовых функций windows и Direct3D */
 #include <windows.h>
 #include <windowsx.h>
 #include <d3d9.h>
@@ -20,15 +20,15 @@ LPDIRECT3DTEXTURE9 saturnTexture; // Указатель на текстуру С
 LPDIRECT3DTEXTURE9 titanTexture; // Указатель на текстуру Титана
 LPDIRECT3DTEXTURE9 ringsTexture; // Указатель на текстуру Колец
 LPDIRECT3DTEXTURE9 skyTexture; // Указатель на текстуру космоса
-
 bool light = TRUE; // Состояние освещения
 LPD3DXMESH titan; // Модель Титана
 LPD3DXMESH sky; // Модель сферы для окружения
 LPD3DXMESH saturn; // Модель Сатурна
-bool mouse = false; // Состояние левой кнопки мыши
+bool mouse_l = false; // Состояние левой кнопки мыши
+bool mouse_r = false; // Состояние правой кнопки мыши
 int ox = 0; // Прошлое значение координаты курсора по оси X
 int oy = 0; // Прошлое значение координаты курсора по оси Y
-int fov = 45;
+int fov = 45; // Угол обзора
 
 D3DXMATRIX matTranslate;  // Матрица параллельного переноса
 D3DXMATRIX matRotateX;    // Матрица поворота по оси X
@@ -44,7 +44,7 @@ float lookDsX = 0.0f;     // Дельта изменения точки обзо
 float lookDsY = 0.0f;     // Дельта изменения точки обзора по оси Y
 
 /* Прототипы функций */
-void initD3D(HWND hWnd);    /* Функция настроийки и инициализации Direct3D */
+void initD3D(HWND hWnd);    /* Функция настройки и инициализации Direct3D */
 void renderFrame(void);     /* Функция отображения одного кадра */
 void cleanD3D(void);        /* Функция закрытия Direct3D и освобождения памяти */
 void initGraphics(void);    /* Функция инициализации графических объектов */
@@ -66,14 +66,13 @@ struct CUSTOMVERTEX {FLOAT X, Y, Z; FLOAT nx, ny, nz; FLOAT tu, tv; DWORD COLOR;
 /* Структура формата вершин для сферы с текстурными координатами */
 typedef struct {
     D3DXVECTOR3 pos;     // Вектор позиции вершины
-    D3DXVECTOR3 norm;    // Вектон нормали вершины
+    D3DXVECTOR3 norm;    // Вектор нормали вершины
     float tu; float tv;  // Текстурные координаты
 } *LPVERTEX;
 #define FVF_VERTEX    D3DFVF_XYZ|D3DFVF_NORMAL|D3DFVF_TEX1
 
 /* Прототип функции обработчика оконных сообщений */
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-
 
 /* Входная точка для программ Windows */
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -82,9 +81,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
                    int nCmdShow) {
     HWND hWnd; // Главное окно
     WNDCLASSEX wc; // Структура класса окна
-
     ZeroMemory(&wc, sizeof(WNDCLASSEX)); // Инициализация структуры нулями
-
     /* Установка параметров класса окна */
     wc.cbSize = sizeof(WNDCLASSEX);
     wc.style = CS_HREDRAW | CS_VREDRAW;
@@ -93,19 +90,16 @@ int WINAPI WinMain(HINSTANCE hInstance,
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
     wc.lpszClassName = "WindowClass";
-
-    RegisterClassEx(&wc); // Реристрация класса окна
-    /* Создание главного окна приложения по зарегестрированному классу */
+    RegisterClassEx(&wc); // Регистрация класса окна
+    /* Создание главного окна приложения по зарегистрированному классу */
     hWnd = CreateWindowEx(0, "WindowClass", "Saturn",
                           WS_OVERLAPPEDWINDOW,
                           300, 300,
                           800, 600,
                           NULL, NULL, hInstance, NULL);
-
     // Отображение окна
     ShowWindow(hWnd, nCmdShow);
     initD3D(hWnd);
-
     MSG msg; // Сообщение
     // Главный цикл обработки сообщений и отрисовки
     while(TRUE) {
@@ -114,40 +108,34 @@ int WINAPI WinMain(HINSTANCE hInstance,
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
-
         // Окончание обработки при получении сообщения завершения работы.
         if(msg.message == WM_QUIT)
             break;
-
         /* Отображение одного кадра */
         renderFrame();
     }
-
     // Освобождение ресурсов
     cleanD3D();
     return msg.wParam;
 }
-
 
 /* Главный обработчик сообщений */
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     // Координаты курсора
     int x;
     int y;
-
     switch(message) {
         case WM_DESTROY: {
                 /* Выход при получении сообщения завершения */
                 PostQuitMessage(0);
                 return 0;
             } break;
-
         /* Обработка нажатия клавиши */
         case WM_KEYDOWN: {
             switch (wParam) {
                 /* Завершение работы при нажатии клавиши Q или Escape */
-                case 'Q': 
-                case VK_ESCAPE: 
+                case 'Q':
+                case VK_ESCAPE:
                     PostQuitMessage(0);
                     break;
                 /* Включение-выключение освещения */
@@ -155,7 +143,6 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
                     light = !light;
                     d3ddev->SetRenderState(D3DRS_LIGHTING, light);
                     break;
-
                 /* Управление углом взгляда через клавиши WASD*/
                 case 'W':
                     lookDX = 0.01f;
@@ -169,7 +156,6 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
                 case 'D':
                     lookDY = -0.01f;
                     break;
-
                 /* Управление углом обзора через клавиши стрелочек */
                 case VK_LEFT:
                     lookDsY = 0.01f;
@@ -183,69 +169,80 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
                 case VK_DOWN:
                     lookDsX = -0.01f;
                     break;
-
                 default:
                     break;
             }
             return 0;
         } break;
-
         /* Обработка отпускания клавиши */
-        case WM_KEYUP: 
+        case WM_KEYUP:
             /* Обнуление всех дельт для остановки изменений */
             lookDX = 0.0f;
             lookDY = 0.0f;
             lookDsX = 0.0f;
             lookDsY = 0.0f;
             break;
-
         /* Обработка нажатия левой кнопки */
         case WM_LBUTTONDOWN:
-            mouse = true;
+            mouse_l = true;
             /* Получение координат курсора */
-            x = lParam & 0x0000FFFF; 
-            y = (lParam & 0xFFFF0000) >> 16; 
+            x = lParam & 0x0000FFFF;
+            y = (lParam & 0xFFFF0000) >> 16;
             
             /* Сохранение положения курсора */
             ox = x;
             oy = y;
             break;
-
         /* Обработка отпускания левой кнопки */
         case WM_LBUTTONUP:
-            mouse = false;
+            mouse_l = false;
             break;
-
+        /* Обработка отпускания правой кнопки */
+        case WM_RBUTTONDOWN:
+            mouse_r = true;
+            /* Получение координат курсора */
+            x = lParam & 0x0000FFFF;
+            y = (lParam & 0xFFFF0000) >> 16;
+            
+            /* Сохранение положения курсора */
+            ox = x;
+            oy = y;
+            break;
+        /* Обработка отпускания правой кнопки */
+        case WM_RBUTTONUP:
+            mouse_r = false;
+            break;
         case WM_MOUSEMOVE:
             /* Обработка только при нажатой левой кнопке */
-            if (!mouse) break;
+            if (!mouse_l && !mouse_r) break;
             /* Получение координат курсора */
-            x = lParam & 0x0000FFFF; 
-            y = (lParam & 0xFFFF0000) >> 16; 
+            x = lParam & 0x0000FFFF;
+            y = (lParam & 0xFFFF0000) >> 16;
             /* Расчёт смещения относительно прошлого положения */
             int dx = ox - x;
             int dy = y - oy;
-
-            /* Увеличение угла поворота точки обзора в соответсвии со смещением курсора*/
-            lookSwX += dy / 500.0f;
-            lookSwY += dx / 500.0f;
+            if (mouse_l) {
+                /* Увеличение угла поворота точки обзора в соответсвии со смещением курсора*/
+                lookSwX += dy / 500.0f;
+                lookSwY += dx / 500.0f;
+            }
+            if (mouse_r) {
+                /* Изменение приближения/отдаления */
+                fov += dx/10.0f;
+            }
             /* Сохранение положения курсора */
             ox = x;
             oy = y;
             break;
     }
-
     /* Стандартная обработка остальных сообщений */
     return DefWindowProc (hWnd, message, wParam, lParam);
 }
 
-
-/* Функция настроийки и инициализации Direct3D */
+/* Функция настройки и инициализации Direct3D */
 void initD3D(HWND hWnd) {
     d3d = Direct3DCreate9(D3D_SDK_VERSION);    // создание интерфейса D3D
-
-    D3DPRESENT_PARAMETERS d3dpp;    // Создание структуры информции устройства
-
+    D3DPRESENT_PARAMETERS d3dpp;    // Создание структуры информации устройства
     ZeroMemory(&d3dpp, sizeof(d3dpp));    // Очистка структуры перед использованием
     d3dpp.Windowed = TRUE;    // Включение оконного режима
     d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // Включение отбрасывания старых кадров
@@ -254,10 +251,8 @@ void initD3D(HWND hWnd) {
     d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
     d3dpp.BackBufferWidth = SCREEN_WIDTH;
     d3dpp.BackBufferHeight = SCREEN_HEIGHT;
-
     d3dpp.EnableAutoDepthStencil = TRUE;
     d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
-
     // Создать класс устройства используя информацию из структуры d3dpp
     d3d->CreateDevice(D3DADAPTER_DEFAULT,
                       D3DDEVTYPE_HAL,
@@ -265,19 +260,16 @@ void initD3D(HWND hWnd) {
                       D3DCREATE_SOFTWARE_VERTEXPROCESSING,
                       &d3dpp,
                       &d3ddev);
-
     initGraphics();
     d3ddev->SetRenderState(D3DRS_ZENABLE, TRUE);    // Включения буфера глубины
     d3ddev->SetRenderState(D3DRS_LIGHTING, TRUE);    // Включение 3D освещения
     d3ddev->SetRenderState(D3DRS_AMBIENT, D3DCOLOR_XRGB(50, 50, 50));    // Окружающий свет
     initLight();
-
     /* Включение и настройка смешивания цветов для реализации прозрачности через альфа-канал */
     d3ddev->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
     d3ddev->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_ADD);
     d3ddev->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
     d3ddev->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
-
     /* Создание моделей */
     sky = CreateMappedSphere(d3ddev, 80.0f, 32, 32);
     titan = CreateMappedSphere(d3ddev, 0.3f, 32, 32);
@@ -285,26 +277,21 @@ void initD3D(HWND hWnd) {
 }
 
 void initLight(void) {
-    D3DLIGHT9 light;    // Создание структуы освещения
+    D3DLIGHT9 light;    // Создание структуры освещения
     D3DMATERIAL9 material;    // Создание структуры материала
-
     ZeroMemory(&light, sizeof(light));    // Очистка структуры освещения
-    light.Type = D3DLIGHT_DIRECTIONAL;    // Установка освщения типа 'направленный свет'
+    light.Type = D3DLIGHT_DIRECTIONAL;    // Установка освещения типа 'направленный свет'
     light.Diffuse = D3DXCOLOR(0.5f, 0.5f, 0.5f, 1.0f);    // Установка цвета света
     light.Direction = D3DXVECTOR3(-1.0f, -0.3f, -1.0f);   // Установка направления света
-
     d3ddev->SetLight(0, &light);    // Установка настроек освещения для источника света #0
     d3ddev->LightEnable(0, TRUE);    // Включение источника света #0
-
     ZeroMemory(&material, sizeof(D3DMATERIAL9));    // Очистка структуры материала
     material.Diffuse = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);    // Установка цвета рассеяния
     material.Ambient = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);    // Установка цвета окружения
-
     d3ddev->SetMaterial(&material);    // Применение настроек материала
 }
 
-void initGraphics(void)
-{
+void initGraphics(void) {
     // Создание буфера вершин
     d3ddev->CreateVertexBuffer(4*sizeof(CUSTOMVERTEX),
                                0,
@@ -312,28 +299,24 @@ void initGraphics(void)
                                D3DPOOL_MANAGED,
                                &vBuffer,
                                NULL);
-
     // Создание массива вершин формата CUSTOMVERTEX для модели плоскости колец
     CUSTOMVERTEX vertices[] = {
         { -3.0f, 3.0f, 0.0f,  0.0f, 0.0f, -1.0f, 0.0f, 0.0f, },
         { 3.0f, 3.0f, 0.0f,   0.0f, 0.0f, -1.0f, 0.0f, 1.0f, },
         { -3.0f, -3.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, },
         { 3.0f, -3.0f, 0.0f,  0.0f, 0.0f, -1.0f, 1.0f, 1.0f, },
-    }; 
-
+    };
     VOID* pVoid;    // Нулевой указатель
-    // Блокировка вершенного буфера и загрузка в него вершин
+    // Блокировка вершинного буфера и загрузка в него вершин
     vBuffer->Lock(0, 0, (void**)&pVoid, 0);
     memcpy(pVoid, vertices, sizeof(vertices));
-    // Разблокировка вершенного буфера
+    // Разблокировка вершинного буфера
     vBuffer->Unlock();
-
     // Загрузка текстур
     D3DXCreateTextureFromFile(d3ddev, "./saturn.jpg", &saturnTexture);
     D3DXCreateTextureFromFile(d3ddev, "./titan.jpg", &titanTexture);
     D3DXCreateTextureFromFile(d3ddev, "./rings1.png", &ringsTexture);
     D3DXCreateTextureFromFile(d3ddev, "./skys.jpg", &skyTexture);
-
     // Отключение режима отсечения граней
     d3ddev->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
 }
@@ -351,7 +334,6 @@ void drawRing() {
     d3ddev->SetTransform(D3DTS_WORLD, &matRotateX);
     // Выбор буфера вершин для отображения
     d3ddev->SetStreamSource(0, vBuffer, 0, sizeof(CUSTOMVERTEX));
-
     setTexture(ringsTexture);
     // Копирование буфера вершин во вторичный буфер
     d3ddev->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
@@ -362,24 +344,20 @@ void drawSky() {
     D3DXMatrixTranslation(&matTranslate, 0.0f, 0.0f, 0.0f);
     /* Применение полученной матрицы */
     d3ddev->SetTransform(D3DTS_WORLD, &matTranslate);
-
     setTexture(skyTexture);
     sky->DrawSubset(0);
 }
-
 
 void drawTitan() {
     // Построение матрицы переноса на 4 единицы по оси X
     D3DXMatrixTranslation(&matTranslate, 4.0f, 0.0f, 0.0f);
     // Построение матрицы поворота основанное на текущем значении поворота
     D3DXMatrixRotationY(&matRotateX, rotation * 1.4f);
-
     /* Перемножение матриц */
     D3DXMATRIX xy = matRotateY * matTranslate * matRotateX;
     /* Применение полученной матрицы */
     d3ddev->SetTransform(D3DTS_WORLD, &xy);
-
-    /* Выбор и настройка текстуры */ 
+    /* Выбор и настройка текстуры */
     setTexture(titanTexture);
     /* Отображение */
     titan->DrawSubset(0);
@@ -390,21 +368,18 @@ void drawSaturn() {
     D3DXMatrixRotationY(&matRotateY, rotation);
     /* Применение полученной матрицы */
     d3ddev->SetTransform(D3DTS_WORLD, &matRotateY);
-
     /* Выбор и настройка текстуры */
     setTexture(saturnTexture);
-
     /* Отображение сферы */
     saturn->DrawSubset(0);
 }
 
 void viewTransform() {
-    D3DXMATRIX matView;    // Матрица транформации представления
+    D3DXMATRIX matView;    // Матрица трансформации представления
     D3DXVECTOR3 camPos = D3DXVECTOR3 (0.0f, 0.0f, 10.0f); // Позиция камеры
     D3DXVECTOR3 lookAt = D3DXVECTOR3 (0.0f, 0.0f, 0.0f);  // Точка направления взгляда
     D3DXVECTOR3 upDir = D3DXVECTOR3 (0.0f, 1.0f, 0.0f);   // Направление "вверх"
     D3DXMatrixLookAtLH(&matView, &camPos, &lookAt, &upDir);
-
     /* Поворот взгляда и точки обзора */
     D3DXMatrixRotationX(&matRotateX, lookRotX);
     D3DXMatrixRotationY(&matRotateY, lookRotY);
@@ -412,17 +387,14 @@ void viewTransform() {
     D3DXMatrixRotationX(&matRotateX, lookSwX);
     D3DXMatrixRotationY(&matRotateY, lookSwY);
     matView = matRotateX * matRotateY * matView;
-
     d3ddev->SetTransform(D3DTS_VIEW, &matView);    // Применение матрицы трансформации представления
-
     D3DXMATRIX matProjection;     // Матрица трансформации проекции
     D3DXMatrixPerspectiveFovLH(&matProjection,
                    D3DXToRadian(fov),    // Угол поля обзора
                    // Соотношение сторон экрана
                    (FLOAT)SCREEN_WIDTH / (FLOAT)SCREEN_HEIGHT,
                    1.0f,       // Ближняя плоскость отсечения
-                   100.0f);    // Дальняя плоскость отсечения 
-
+                   100.0f);    // Дальняя плоскость отсечения
     d3ddev->SetTransform(D3DTS_PROJECTION, &matProjection);    // Применение трансформации проекции
 }
 
@@ -431,9 +403,7 @@ void renderFrame(void) {
     /* Очистка экрана и буфера глубины */
     d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
     d3ddev->Clear(0, NULL, D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
-
     d3ddev->BeginScene();    // Начало 3D сцены
-
     // Выбор формата вершин
     d3ddev->SetFVF(CUSTOMFVF);
     
@@ -442,23 +412,19 @@ void renderFrame(void) {
     lookRotY += lookDY; // Увеличение угла взгляда по Y
     lookSwX += lookDsX; // Увеличение угла поворота точки обзора по оси X
     lookSwY += lookDsY; // Увеличение угла поворота точки обзора по оси Y
-
     viewTransform();   // Трансформация матрицы представления и проекции
-
     /* Отображение объектов */
     drawSky();
     drawSaturn();
-    drawTitan(); 
+    drawTitan();
     drawRing();
-
-    d3ddev->EndScene();    // Окончание 3D сцены 
+    d3ddev->EndScene();    // Окончание 3D сцены
     d3ddev->Present(NULL, NULL, NULL, NULL);   // Отображение созданного кадра на экране
 }
 
-
 /* Функция освобождения ресурсов Direct3D и COM */
 void cleanD3D(void) {
-    vBuffer->Release();    // Закрытие и освобождение вершенного буфера
+    vBuffer->Release();    // Закрытие и освобождение вершинного буфера
     d3ddev->Release();    // Закрыть 3D устройство
     d3d->Release();    // Закрыть Direct3D
 }
@@ -472,14 +438,11 @@ LPD3DXMESH CreateMappedSphere(LPDIRECT3DDEVICE9 pDev,float fRad,UINT slices,UINT
     if (FAILED(mesh->CloneMeshFVF(D3DXMESH_SYSTEMMEM,FVF_VERTEX,pDev,&texMesh))) return mesh;
     // Изначальная модель не нужна - освобождаем
     mesh->Release();
-
     // Блокировка буфера вершин
     LPVERTEX pVerts;
     if (SUCCEEDED(texMesh->LockVertexBuffer(0,(void **) &pVerts))) {
-
         // Получение количества вершин
         int numVerts=texMesh->GetNumVertices();
-
         // Цикл по всем вершинам
         for (int i=0;i<numVerts;i++) {
             // Расчёт текстурных координат
@@ -488,7 +451,7 @@ LPD3DXMESH CreateMappedSphere(LPDIRECT3DDEVICE9 pDev,float fRad,UINT slices,UINT
             // Переход к следующей вершине
             pVerts++;
         }
-        // Разблокирока буфера вершин
+        // Разблокировка буфера вершин
         texMesh->UnlockVertexBuffer();
     }
     // Возврат указателя на результат
