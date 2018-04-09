87943c89ab5008f23166eda18d5ed2ef0f2c9fa0
tikhomirovsvl <tikhomirovsvl@mosreg.ru>
2018-04-09 11:50:36 +0300

    [86] add delay proc

--- asm86/runstr.asm
+++ asm86/runstr.asm
@@ -1,18 +1,32 @@
 .386
 RomSize    EQU   4096
 
-Code       SEGMENT use16
-           ASSUME cs:Code,ds:Code,es:Code
+Stk        SEGMENT AT 100h use16
+; размер стека
+           dw    100 dup (?)
+StkTop     Label Word
+Stk        ENDS
 
-           ;Образы десятичных цифр от 0 до 9
-Image      db    03Fh,00Ch,076h,05Eh,04Dh,05Bh,07Bh,00Eh,07Fh,05Fh
+Data       SEGMENT AT 40h use16
+;Здесь размещаются описания переменных
            ;бегущая строка
 string     db    01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h, 01h 
+Data       ENDS
+
+Code       SEGMENT use16
+           ASSUME cs:Code,ds:Data,es:Data
+
+;Здесь размещаются описания констант
+           ;Образы десятичных цифр от 0 до 9
+Image      db    03Fh,00Ch,076h,05Eh,04Dh,05Bh,07Bh,00Eh,07Fh,05Fh
 
 Start:
-           mov   ax,Code
+           mov   ax,Data
            mov   ds,ax
            mov   es,ax
+           mov   ax,Stk
+           mov   ss,ax
+           lea   sp,StkTop
 
 ; считываем скорость движения строки с ацп
 
@@ -35,6 +49,17 @@ InfLoop:
            
            jmp   InfLoop
 
+
+
+Delay      proc  near ; param cx=count
+           push  cx
+DelayLoop:
+           inc   cx
+           dec cx
+           loop  DelayLoop
+           pop   cx
+           ret
+Delay      endp
 
 
 ;В следующей строке необходимо указать смещение стартовой точки
