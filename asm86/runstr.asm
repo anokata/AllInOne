fbbe32d59c2a7cc424865bad5fafdc3a8fd921ed
anokata <anokata8@mail.ru>
2018-04-11 22:09:57 +0300

    [86]

--- asm86/runstr.asm
+++ asm86/runstr.asm
@@ -10,12 +10,12 @@ Stk        ENDS
 Data       SEGMENT at 0 use16
 ;Здесь размещаются описания переменных
            ;бегущая строка 10
-string     db   10 dup (1) 
-delayc     db   1
+string     db   10 dup (?) 
+delayc     db   ?
 Data       ENDS
 
 Code       SEGMENT use16
-           ASSUME cs:Code,ds:Data,es:Data
+           ASSUME cs:Code,ds:Data,es:Code
 
 ;Здесь размещаются описания констант
            ;Образы десятичных цифр от 0 до 9
@@ -24,37 +24,47 @@ Image      db    03Fh,00Ch,076h,05Eh,04Dh,05Bh,07Bh,00Eh,07Fh,05Fh
 Start:
     mov   ax, Data
     mov   ds, ax
+    mov   ax, Code
     mov   es, ax
     mov   ax, Stk
     mov   ss, ax
     lea   sp, StkTop
+
+    mov   di, 11 
+    mov   al, 0
+    FillLoop:
+    mov   string+di, al
+    dec   di
+    jnz   FillLoop
+
     mov   di, 0 ; index
+    mov   dx, 0
 
 InfLoop:
     lea   bx, Image    ;bx - указатель на массив образов
-    lea   dx, string+di   ; загружаем значение цифры из стоки
+    mov   dl, string+di   ; загружаем значение цифры из стоки
     ; index pos
     add   bx, dx      ; вычисляем адрес образа цифры
-    mov   al,[bx]     ; Выводим цифру на индикатор
+    mov   al,es:[bx]     ; Выводим цифру на индикатор
     out   0, al
     ; вторая цифра
     lea   bx, Image
     add   bx, dx
     inc   bx
-    mov   al,[bx]     ; Выводим цифру на индикатор
+    mov   al,es:[bx]     ; Выводим цифру на индикатор
     out   1,al
     ; третья цифра
     lea   bx, Image
     add   bx, dx
     inc   bx
     inc   bx
-    mov   al,[bx]     ; Выводим цифру на индикатор
+    mov   al,es:[bx]     ; Выводим цифру на индикатор
     out   2,al
     ; четвертая цифра
     lea   bx, Image
     add   bx, dx
     add   bx, 3
-    mov   al, [bx]     ; Выводим цифру на индикатор
+    mov   al, es:[bx]     ; Выводим цифру на индикатор
     out   3, al
 
 ; считываем скорость движения строки с ацп
@@ -93,12 +103,16 @@ Savedi:
 
 
 Delay      proc  near ; param cx=count
+           mov   ax, 10
+LoopTen:
            push  cx
 DelayLoop:
            inc   cx
-           dec cx
+           dec   cx
            loop  DelayLoop
            pop   cx
+           dec   ax
+           jnz   LoopTen
            ret
 Delay      endp
 
