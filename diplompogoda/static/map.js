c9d67daf72c283fe52721e8a3d6f49dafa3599a8
anokata <anokata8@mail.ru>
2019-05-27 12:46:23 +0300

    Change to canvas

--- diplompogoda/static/map.js
+++ diplompogoda/static/map.js
@@ -1,24 +1,35 @@
 window.onload = function () {
 
-var width, height, svg, path, projection;
+const CITY_MIN_DIST = 0.03;
+const MOUSE_PAUSE = 200;
+const SCALE_VAL = 50;
+var width, height, path, projection;
 var sens = 0.25;
 var colors = ["#883", "#833", "#883", "#383", "#338", "#830", "#380"];
 // Элемент всплывающей подсказки
 var tooltip = d3.select("body").append("div").attr("class", "tooltip");
 var tempById = {}; // DEL?
-var addition_towns = [];
+var context, geoGenerator;
+var world, cities, lakes, rivers;
+var mouse_timer, mouse_point, mouse_xy;
+var tooltip_timer;
+var isDragging = false;
+var startingPos = [];
 
 
 // Подпрограмма показывающая данные для выбранного города
-function renderCities(city) {
+function renderCities(city, lon, lat) {
     wheather_data = {};
-    city_data = cities[city];
-    console.log(city_data);
-    lat = city_data[0];
-    lon = city_data[1];
+    if (!lon || !lat) {
+        city_data = cities_coords[city];
+        lat = city_data[0];
+        lon = city_data[1];
+    }
+    console.log(city, lon, lat);
+    // TODO
     add_selected_city(city, lat, lon);
     send(wheather_data, city, lat, lon, view);
-    refresh_projection();
+    update();
 }
 
 function add_selected_city(city, lat, lon) {
@@ -32,53 +43,35 @@ function add_selected_city(city, lat, lon) {
             "coordinates": [lon, lat]
         }
     }
-    addition_towns.push(town);
-    console.log(addition_towns);
-    add_cities(addition_towns);
+    cities = cities.concat([town]);
 }
 
+function scale_projection(value) {
+    projection.scale(projection.scale() + value);
+    update();
+}
 
-    function refresh_projection() {
-            svg.selectAll("path.points").attr("d", path2);
-            svg.selectAll("path.points_dot").attr("d", path);
-            svg.selectAll("path.land").attr("d", path);
-            svg.selectAll("path.temp").attr("d", path);
-            svg.selectAll("path.water").attr("d", path);
-            svg.selectAll("path.lakes").attr("d", path);
-            svg.selectAll("path.rivers").attr("d", path);
-    }
-
-    function scale_projection(value) {
-        projection.scale(projection.scale() + value);
-        refresh_projection();
-    }
-
-    function init() {
+function init() {
         setMap();
-        $("#scaleup").on("click", function() { scale_projection(30) });
-        $("#scaledown").on("click", function() { scale_projection(-30) });
+        $("#scaleup").on("click", function() { scale_projection(SCALE_VAL) });
+        $("#scaledown").on("click", function() { scale_projection(-SCALE_VAL) });
         $("#map").bind('mousewheel DOMMouseScroll', function(event){
             if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
             console.log('scrolling');
-                scale_projection(15);
+                scale_projection(SCALE_VAL/2);
             }
             else {
             console.log('scrolling');
-                scale_projection(-15);
+                scale_projection(-SCALE_VAL/2);
             }
         });
-    }
+}
 
-  // Подпрограмма настройки карты
-  function setMap() {
+// Подпрограмма настройки карты
+function setMap() {
     // Высота и ширина карты
     width = 1000, height = 800;
      
-    // Создание и настройка SVG контейнера карты
-    svg = d3.select('#map').append('svg')
-        .attr('width', width)
-        .attr('height', height);
-
     // Создание объекта отрогональной проекции
     projection = d3.geo.orthographic()
         .scale(380)
@@ -92,19 +85,88 @@ function add_selected_city(city, lat, lon) {
     path.pointRadius(1.5);
     path2.pointRadius(6.0);
 
-    // Добавление круга океанов
-    svg.append("path")
-        .datum({type: "Sphere"})
-        .attr("class", "water")
-        .attr("d", path);
-
+    context = d3.select('#map canvas')
+      .node()
+      .getContext('2d', { alpha: false });
+    d3.select('#map canvas')
+        .attr('width', width)
+        .attr('height', height);
+    geoGenerator = d3.geoPath()
+      .projection(projection)
+      .context(context);
+    geoGenerator.pointRadius(1.5);
 
     // Загрузка данных
     loadData();
-  }
-   
-  // Подпрограмма загрузки геоданных
-  function loadData() {
+}
+
+function update() {
+    context.fillStyle = "#333";
+    //context.clearRect(0, 0, width, height);
+    context.fillRect(0, 0, width, height);
+    context.lineWidth = 2.0;
+    context.strokeStyle = '#000';
+
+    // Сфера воды
+    context.fillStyle = "#234c75";
+    context.beginPath();
+    geoGenerator({type: 'Sphere'});
+    context.stroke();
+    context.fill();
+
+    var geojson = world.features;
+    for (var i = 0; i < geojson.length; i++) {
+        var country = geojson[i];
+        context.fillStyle = get_color(country);
+        context.beginPath();
+        geoGenerator({type: 'FeatureCollection', features: [country]})
+        context.stroke();
+        context.fill();
+    }
+
+    var geojson = rivers;
+    context.strokeStyle = '#00f';
+    context.lineWidth = 0.5;
+    context.beginPath();
+    for (var i = 0; i < geojson.length; i++) {
+        var river = geojson[i];
+        geoGenerator({type: 'FeatureCollection', features: [river]})
+    }
+    context.stroke();
+
+    var geojson = lakes;
+    context.strokeStyle = '#00f';
+    context.lineWidth = 0.5;
+    context.fillStyle = "#234c75";
+    context.beginPath();
+    for (var i = 0; i < geojson.length; i++) {
+        var lake = geojson[i];
+        geoGenerator({type: 'FeatureCollection', features: [lake]})
+    }
+    context.fill();
+    draw_cities(cities);
+}
+
+function draw_cities(geojson) {
+    context.strokeStyle = '#eee';
+    context.lineWidth = 0.5;
+    context.fillStyle = "#eee";
+    context.beginPath();
+    for (var i = 0; i < geojson.length; i++) {
+        var city = geojson[i];
+        geoGenerator({type: 'FeatureCollection', features: [city]})
+    }
+    context.fill();
+}
+
+function get_color(d) { 
+    var c = d.properties.MAPCOLOR7 || 0;
+    var n = Math.abs(c % colors.length);
+    return colors[n]; 
+}
+
+// Подпрограмма загрузки геоданных
+function loadData() {
     // Запрос геоданных границ в topoJSON-формате и точек городов
     queue()
       .defer(d3.json, "static/geo/topoworld.json")  
@@ -112,187 +174,163 @@ function add_selected_city(city, lat, lon) {
       .defer(d3.json, "static/geo/topolakes.json")  
       .defer(d3.json, "static/geo/toporivers.json")  
       .await(processData);  // обработка загруженных данных
-  }
+}
    
-  // Подпрограмма обработки загруженных геоданных
-  function processData(error, worldMap, cityMap, lakesMap, riversMap) {
+// Подпрограмма обработки загруженных геоданных
+function processData(error, worldMap, cityMap, lakesMap, riversMap) {
     if (error) return console.error(error);
-    console.log(worldMap);
-    console.log(cityMap);
-    var world = topojson.feature(worldMap, worldMap.objects.world);
-    var cities = topojson.feature(cityMap, cityMap.objects.citymini).features;
-    var lakes = topojson.feature(lakesMap, lakesMap.objects.lakes).features;
-    var rivers = topojson.feature(riversMap, riversMap.objects.rivers).features;
+    // console.log(worldMap);
+    // console.log(cityMap);
+    world = topojson.feature(worldMap, worldMap.objects.world);
+    cities = topojson.feature(cityMap, cityMap.objects.citymini).features;
+    lakes = topojson.feature(lakesMap, lakesMap.objects.lakes).features;
+    rivers = topojson.feature(riversMap, riversMap.objects.rivers).features;
     countries = world.features;
     console.log("Мир", world);
     console.log("Города", cities);
     console.log("Озёра", lakes);
     console.log("Реки", rivers);
 
-      // Добавление границ стран
-    var world2 = svg.selectAll("path.land")
-        .data(countries)
-        .enter().append("path")
-        .attr("class", "land")
-        .attr("stroke-width", 1.5)
-        .attr("stroke", "black")
-        .attr("fill", function (d) { 
-            var c = d.properties.MAPCOLOR7 || 0;
-            var n = Math.abs(c % colors.length);
-            return colors[n]; })
-        .attr("d", path);
-
+    update();
       // Обработчик поворода шара
-    svg.selectAll("path")
+    d3.selectAll("canvas")
         .call(d3.behavior.drag()
         .origin(function() { var r = projection.rotate(); return {x: r[0] / sens, y: -r[1] / sens}; })
         .on("drag", function() {
             var rotate = projection.rotate();
             projection.rotate([d3.event.x * sens, -d3.event.y * sens, rotate[2]]);
-            refresh_projection();
-            //console.log(path.centroid());
-            //svg.selectAll("text.temp").attr("x", );
+            update();
+            console.log("drag");
       }));
 
-    add_cities(cities);
 
-    // Добавление озёр
-    var lakes_svg = svg.selectAll("path.lakes")
-        .data(lakes)
-        .enter().append("path")
-        .attr("class", "lakes")
-        .attr("stroke-width", 0)
-        .attr("fill", "#234c75")
-        .attr("d", path);
-
-    // Добавление рек
-    var rivers_svg = svg.selectAll("path.rivers")
-        .data(rivers)
-        .enter().append("path")
-        .attr("class", "rivers")
-        .attr("stroke-width", 1)
-        .attr("stroke", "#234c75")
-        .attr("fill", "none")
-        .attr("d", path);
-
-
-      /*
-    svg.selectAll(".temp")
-        .data(cities)
-        .enter().append("text")
-        .attr("d", path)
-        .attr("class", "temp")
-        .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-        .attr("dy", ".35em")
-        .text(function(d) { return d.properties.name; });
-        */
-
-      /*
-    var pointsd = 
-[
-  {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [
-        38.5,
-        58.5
-      ]
-    }
-  }
-];
-    svg.selectAll("path.temp")
-        .data(pointsd)
-        .enter().append("path")
-        .attr("class", "temp")
-        .attr("stroke-width", 5)
-        .attr("stroke", "white")
-        .attr("fill", "white")
-        .attr("d", path);
-
-    svg.selectAll("text")
-        .data(pointsd)
-        .enter()
-        .append("text")
-        .attr("transform", function(d) { return "translate(" + 
-                projection([d.geometry.coordinates[0], d.geometry.coordinates[1]]) + ")" })
-        .attr("class", "temp")
-        .text("+88")
-        .attr("x", function(d) { console.log(d); return d.geometry.coordinates[0]; })
-        .attr("y", function(d) { return d.geometry.coordinates[1]; })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "20px")
-        .attr("fill", "red")
-      ;
-  */
-  }
-   
-  // Вызов главной функции
-  init();
-
-function add_cities(cities) {
-    // Добавление точек городов
-    var citysvg = svg.selectAll("path.points_dot")
-        .data(cities)
-        .enter().append("path")
-        .attr("class", "points_dot")
-        .attr("d", path);
-
-    var citysvg = svg.selectAll("path.points")
-        .data(cities)
-        .enter().append("path")
-        .attr("class", "points")
-        .attr("d", path2);
-
-    // Установка обработчика при наведении мыши показывающего подсказку с данными
-    citysvg.on("mouseover", function(d) {
-        // Получение координат выбранной точки
-        lat = d.geometry.coordinates[1];
-        lon = d.geometry.coordinates[0];
-        // Формирование текста с данными
-        var text = "";
-        text += d.properties.name_ru || "";
-        text += "<br/>";
-        var wheather_data = {};
-        var city = d.properties.name_ru;
-        // Получение погодных данных в выбранной точке
-        send(wheather_data, d.properties.name_ru, lat, lon, function (wheather_data) {
-              // Обработка погодных данных и формирование текста для сводки
-              wheather_data["city"] = d.properties.name_ru;
-              // Вывод текста с данными на веб-странице
-              //view(wheather_data);
-              $("#cities").val(city);
-              //renderCities(city);
-              // Формирование текста с погодной сводкой
-			  text += "<img class='tooltip_img' src='" + "https://yastatic.net/weather/i/icons/blueye/color/svg/" + wheather_data[city]["icon"] + ".svg" + "'/>";
-			  text += human_condition(wheather_data[city]["condition"]);
-			  text += "<br/>";
-              text += "Температура: ";
-              text += human_temp(wheather_data[d.properties.name_ru]["temp"]) + "°";
-              text += "<br/>";
-              text += "Влажность: ";
-              text += wheather_data[d.properties.name_ru]["humidity"] + "%";
-              text += "<br/>";
-              text += "Ветер: ";
-              text += human_wind(wheather_data[city]["wind_dir"], wheather_data[city]["wind_speed"]);
-              text += "<br/>";
-              // Настройка всплывающей подсказки
-              tooltip.html(text)
-                  .style("left", (d3.event.pageX + 7) + "px")
-                  .style("top", (d3.event.pageY - 15) + "px")
-                  .style("display", "block")
-                  .style("opacity", 1)
-                .on("mousemove", function(d) {
-                      tooltip.style("left", (d3.event.pageX + 7) + "px")
-                      .style("top", (d3.event.pageY - 15) + "px");
-                });
+    $("canvas")
+        .mousedown(function (evt) {
+            isDragging = false;
+            startingPos = [evt.pageX, evt.pageY];
+        })
+        .mousemove(function (evt) {
+            if (!(evt.pageX === startingPos[0] && evt.pageY === startingPos[1])) {
+                isDragging = true;
+            }
         });
+
+    d3.selectAll("canvas")
+      .on("mousemove", function () {
+          // Если совпадёт с городом
+          //console.log(projection.invert(d3.mouse(this)));
+          clearTimeout(mouse_timer);
+          mouse_timer = setTimeout(mouse_stopped, MOUSE_PAUSE);
+          mouse_point = get_mouse_geopoint(this);
+          mouse_xy = d3.mouse(this);
+          tooltip_hide();
+          clearTimeout(tooltip_timer);
+      })
+    d3.selectAll("canvas")
+      .on("mouseup", function () {
+          var mouse_point = get_mouse_geopoint(this);
+          // Если совпадёт с городом c определённой точностью
+          // расстояние между точками гео
+          // Вычислим расстояние между точкой мыши и каждым городом
+          var nearest = nearest_city(mouse_point);
+          if (nearest) {
+              console.log(nearest, nearest.properties.name_ru);
+              renderCities(nearest.properties.name_ru, nearest.geometry.coordinates[0], nearest.geometry.coordinates[1]);
+          } else {
+            if (!isDragging) {
+                show_wheather_data("", mouse_point[0], mouse_point[1]);
+                // TODO Если есть населённый пунк рядом то подробно
+            }
+          }
+          isDragging = false;
+          startingPos = [];
+      })
+      // Добавление границ стран
+
+}
+   
+function get_mouse_geopoint(self) {
+          var lon, lat;
+          lon = projection.invert(d3.mouse(self))[0];
+          lat = projection.invert(d3.mouse(self))[1];
+          //console.log(lon, lat);
+          return [lon, lat];
+}
+
+// Подпрограмма поиска ближайшего города
+function nearest_city(p) {
+    var nearest;
+    var min_distance = 10000000;
+    for (i = 0; i < cities.length; i++) {
+        var city = cities[i];
+        var city_point = [city.geometry.coordinates[0], city.geometry.coordinates[1]]
+        var dist = d3.geo.distance(p, city_point);
+        if (min_distance > dist) {
+            min_distance = dist;
+            nearest = city;
+        }
+    }
+    //console.log(min_distance, nearest);
+    if (min_distance < CITY_MIN_DIST) {
+        return nearest;
+    } else return false;
+}
+
+function mouse_stopped() {
+    console.log('stop', mouse_point);
+    var nearest = nearest_city(mouse_point);
+    if (nearest) {
+        console.log(nearest, nearest.properties.name_ru);
+        make_wheather_text(nearest);
+    }
+}
+
+
+function make_wheather_text(city) {
+    var lon = city.geometry.coordinates[0]; 
+    var lat = city.geometry.coordinates[1];
+    var city_name = city.properties.name_ru;
+    $("#cities").val(city_name);
+    show_wheather_data(city_name, lon, lat);
+}
+
+function show_wheather_data(city_name, lon, lat) {
+    // Формирование текста с данными
+    var text = "";
+    text += city_name;
+    text += "<br/>";
+    var wheather_data = {};
+    // Получение погодных данных в выбранной точке
+    send(wheather_data, city_name, lat, lon, function (wheather_data) {
+          // Обработка погодных данных и формирование текста для сводки
+          wheather_data["city"] = city_name;
+          // Формирование текста с погодной сводкой
+          text += "<img class='tooltip_img' src='" + "https://yastatic.net/weather/i/icons/blueye/color/svg/" + wheather_data[city_name]["icon"] + ".svg" + "'/>";
+          text += human_condition(wheather_data[city_name]["condition"]);
+          text += "<br/>";
+          text += "Температура: ";
+          text += human_temp(wheather_data[city_name]["temp"]) + "°";
+          text += "<br/>";
+          text += "Влажность: ";
+          text += wheather_data[city_name]["humidity"] + "%";
+          text += "<br/>";
+          text += "Ветер: ";
+          text += human_wind(wheather_data[city_name]["wind_dir"], wheather_data[city_name]["wind_speed"]);
+          text += "<br/>";
+          // Настройка всплывающей подсказки
+          tooltip.html(text)
+              .style("left", (mouse_xy[0] + 33) + "px")
+              .style("top", (mouse_xy[1] + 47) + "px")
+              .style("display", "block")
+              .style("opacity", 1);
+          tooltip_timer = setTimeout(tooltip_hide, 5000);
     });
-    // Обработчик скрытия подсказки
-    citysvg.on("mouseout", function(d) {
-              tooltip.style("opacity", 0)
-              .style("display", "none");
-            })
+}
+
+// Обработчик скрытия подсказки
+function tooltip_hide(){
+    tooltip.style("opacity", 0)
+        .style("display", "none");
 }
 
 // конфигурации поля ввода городов с автодополнением
@@ -310,6 +348,10 @@ $("#cities").keypress(function(e){
         renderCities(city);
     }
 });
+
+
+// Вызов главной функции
+init();
 
 };
 
--- diplompogoda/static/towns.js
+++ diplompogoda/static/towns.js
@@ -2,6 +2,6 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', "static/towns.json", false);
 
 xhr.onload = function() {
-    window.cities = JSON.parse(this.responseText);
+    window.cities_coords = JSON.parse(this.responseText);
     };
 xhr.send(); 
