10885277cf6ad0f832b8aae4b060dab5f1ece0a1
anokata <anokata8@mail.ru>
2019-05-28 12:09:18 +0300

    12 h

--- diplompogoda/static/wheather.js
+++ diplompogoda/static/wheather.js
@@ -56,7 +56,7 @@ function view(wheather_data) {
 	$("#uv_index").text(wheather_data[city].forecasts[0].parts.day.uv_index);
 
     // TODO С какого часу?
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 13; i++) {
         $("#hour_" + i).text(human_temp(wheather_data[city].forecasts[0].hours[i].temp));
         // $("#hour_" + i + "_cond").text(human_condition(wheather_data[city].forecasts[0].hours[i].condition));
         $("#hour_" + i + "_icon").attr("src", make_icon(wheather_data[city].forecasts[0].hours[i].icon));
