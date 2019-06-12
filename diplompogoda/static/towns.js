96d54bfbdfda661ab110d8b95a4eaa02814e959a
anokata <anokata8@mail.ru>
2019-06-12 15:45:58 +0300

    text

--- diplompogoda/static/towns.js
+++
@@ -1,7 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', "static/towns.json", false);
-
-xhr.onload = function() {
-    window.cities_coords = JSON.parse(this.responseText);
-    };
-xhr.send(); 
