0a6972735694e2c1d0ac67c646db36c9d8004011
anokata <anokata8@mail.ru>
2019-06-28 19:23:18 +0300

    add corona folder for android dev

---
+++ corona/takeone/main.lua
@@ -0,0 +1,18 @@
+local background = display.newImageRect( "background.png", 350, 570 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local platform = display.newImageRect( "platform.png", 300, 50 )
+platform.x = display.contentCenterX
+platform.y = display.contentHeight-25
+
+local balloon = display.newImageRect( "balloon.png", 112, 112 )
+balloon.x = display.contentCenterX
+balloon.y = display.contentCenterY
+balloon.alpha = 0.8
+
+local physics = require( "physics" )
+physics.start()
+
+physics.addBody( platform, "static" )
+physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
