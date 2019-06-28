a37a611b688ba209e5d4fcaff7eb796d168bc890
Rainlin <rlinnyaka@gmail.com>
2019-06-28 17:31:14 +0300

    ball

--- java/android/tamagochi/make.sh
+++ java/android/tamagochi/make.sh
@@ -2,17 +2,20 @@
 SOURCE=ru/ksi
 BASE=/usr/lib
 SDK="/mnt/store/android/sdk"
-BUILD_TOOLS="${SDK}/build-tools/20.0.0"
-PLATFORM="${SDK}/platforms/android-20"
+SDK="/media/ksi/ANIMA/androidsdk/"
+VER=29
+BUILD_TOOLS="${SDK}/build-tools/${VER}.0.0"
+PLATFORM="${SDK}/platforms/android-${VER}"
 TARGET=Ksi
-SIGNER=${SDK}/build-tools/29.0.0/apksigner
+SIGNER=${SDK}/build-tools/${VER}.0.0/apksigner
+#IS_R_PC=$()
 
 rm -rf build/*
 mkdir -p build/gen build/obj build/apk
 "${BUILD_TOOLS}/aapt" package -f -m -J build/gen/ -S res -M AndroidManifest.xml -I "${PLATFORM}/android.jar"
 javac -source 1.7 -target 1.7 -bootclasspath "${JAVA_HOME}/jre/lib/rt.jar" \
 		 -classpath "${PLATFORM}/android.jar" -d build/obj \
-		 build/gen/${SOURCE}/R.java java/${SOURCE}/MainActivity.java
+		 build/gen/${SOURCE}/R.java java/${SOURCE}/*.java
 "${BUILD_TOOLS}/dx" --dex --output=build/apk/classes.dex build/obj/
 "${BUILD_TOOLS}/aapt" package -f -M AndroidManifest.xml -S res/  -I "${PLATFORM}/android.jar" \
 		-F build/${TARGET}.unsigned.apk build/apk/
