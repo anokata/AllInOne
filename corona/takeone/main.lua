afc79d1b477d6ca6be07bc8e268be83a5fb00fe9
anokata <anokata8@mail.ru>
2019-07-06 17:42:52 +0300

    lib

--- corona/takeone/main.lua
+++ corona/takeone/main.lua
@@ -2,6 +2,17 @@ local composer = require( "composer" )
 local physics = require( "physics" )
 local widget = require( "widget" )
 saveGame = {}
+
+x = 'string'
+string_mt = getmetatable(x)
+string_mt.__add = function(a, b)
+ return a..b
+end
+string_mt.__index = function(s, i)
+ return string.format('%c', string.byte(s, i))
+end
+
+
 widget.setTheme( "widget_theme_android_holo_light" )
 liba = require("liba")
 display.setStatusBar( display.HiddenStatusBar )
