27fb874e241256c09a97fe24056cf749af5f1c10
anokata <anokata8@mail.ru>
2019-09-22 15:46:48 +0300

    sort

--- algo/sort/insert_sort.py
+++ algo/sort/insert_sort.py
@@ -5,6 +5,14 @@
 test1 = [5,4,3,2,1,8,4,3,2,2,1,8]
 
 def insert_sort(a):
+    for j in range(2, len(a)):
+        current = a[j]
+        i = j-1
+        while i >= 0 and a[i] > current: # >=
+            print(a)
+            a[i+1] = a[i]
+            i -= 1
+        a[i+1] = current
     return a
 
 print(test1)
