e35bf5b93c00fb2391e4a55412f9df3c8ab8b7dd
anokata <anokata8@mail.ru>
2019-10-25 19:50:19 +0300

    beer

---
+++ java/2019/Beer.java
@@ -0,0 +1,12 @@
+public class Beer {
+    public static void main(String[] args) {
+//99 bottles of beer on the wall, 99 bottles of beer.
+//Take one down, pass it around, 98 bottles of beer on the wall... 
+//No more bottles of beer on the wall, no more bottles of beer.
+//We've taken them down and passed them around; now we're drunk and passed out! 
+
+    }
+}
+
+class Strings {
+}
