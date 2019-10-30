d3a6c2852900154baa243170578ff094e059fcc4
anokata <anokata8@mail.ru>
2019-10-30 15:32:07 +0300

    beer

--- java/2019/Beer.java
+++ java/2019/Beer.java
@@ -1,12 +1,39 @@
 public class Beer {
     public static void main(String[] args) {
-//99 bottles of beer on the wall, 99 bottles of beer.
-//Take one down, pass it around, 98 bottles of beer on the wall... 
-//No more bottles of beer on the wall, no more bottles of beer.
-//We've taken them down and passed them around; now we're drunk and passed out! 
-
+        Bottles bottles = new Bottles(3);
+        while (bottles.isMore()) {
+            System.out.print(bottles.takeOne());
+        }
     }
 }
 
-class Strings {
+class Bottles {
+    public static String linebeer = " bottles of beer ";
+    public static String linewall = " on the wall";
+    public static String linetake = "Take one down, pass it around";
+    public static String lineno   = "No more bottles of beer on the wall, no more bottles of beer.";
+    private int count;
+
+    public Bottles(int count) {
+        this.count = count;
+    }
+
+    private String formatLine(String line) {
+        return line + "\n";
+    }
+
+    public String takeOne() {
+        count--;
+        if (count == 0) {
+            return lineno;
+        }
+        String beer;
+        beer = formatLine(count + linebeer + linewall);
+        beer += formatLine(linetake);
+        return beer;
+    }
+
+    public Boolean isMore() {
+        return count > 0;
+    }
 }
