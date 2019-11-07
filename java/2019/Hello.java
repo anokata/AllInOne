dc8ed96b6414dcce80bce6a81355be2b215b4a1a
anokata <anokata8@mail.ru>
2019-11-07 15:51:48 +0300

    hi

--- java/2019/Hello.java
+++ java/2019/Hello.java
@@ -1,5 +1,21 @@
-public class Hello {
+class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Java world, writed in vim by memory.");
+        // int types
+        byte a;
+        short b;
+        int c;
+        long d;
+        // real types
+        float f = 3f;
+        double e = 4;
+        a = 300%2;
+        p("initialized " + a);
+        // other primitives
+        boolean flag = true;
+        char sym = 'c';
+        p("Hello Java world, writed in vim by memory.");
+    }
+    static void p(String s) {
+        System.out.println(s);
     }
 }
