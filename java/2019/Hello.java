4afc5b6d7f42dd45b1f401730baf330210762f5e
anokata <anokata8@mail.ru>
2019-11-08 09:17:02 +0300

    arrays

--- java/2019/Hello.java
+++ java/2019/Hello.java
@@ -14,8 +14,22 @@ class Hello {
         boolean flag = true;
         char sym = 'c';
         p("Hello Java world, writed in vim by memory.");
+        int[] nums; // nums is link to Array of ints
+        nums = new int[7]; // create Array of ints
+        Sock[] socks; // array of socks
+        socks = new Sock[8];
+        socks[0] = new Sock("z");
+        p(socks[0].name);
+        if (socks[1] == null) { p("null");}
     }
     static void p(String s) {
         System.out.println(s);
+    }
+}
+
+class Sock {
+    String name;
+    Sock(String s) {
+        name = s;
     }
 }
