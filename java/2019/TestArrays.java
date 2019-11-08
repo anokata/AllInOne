219c1e6cef2b6f6a28a971328efd70351f6e49b6
anokata <anokata8@mail.ru>
2019-11-08 16:45:19 +0300

    tri

---
+++ java/2019/TestArrays.java
@@ -0,0 +1,22 @@
+class TestArrays {
+    public static void main(String[] args) {
+        int y = 0;
+        String [] islands = new String[4];
+        int [] index = new int[4];
+        index[0] = 1;
+        index[1] = 3;
+        index[2] = 0;
+        index[3] = 2;
+        islands[0] = "Бермуды";
+        islands[1] = "Фиджи";
+        islands[2] = "Азорские острова";
+        islands[3] = "Косумель";
+
+        int ref;
+        while (y < 4) {
+            ref = index[y];
+            System.out.println(islands[ref]);
+            y = y + 1;
+        }
+    }
+}
