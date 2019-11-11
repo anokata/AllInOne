7098c894f612f2699b90c24b515af6f8d1a8538e
anokata <anokata8@mail.ru>
2019-11-11 10:22:59 +0300

    startdot

---
+++ java/2019/simpledotcomgame/SimpleDotCom.java
@@ -0,0 +1,10 @@
+class SimpleDotCom {
+    private int[] locationCells;
+    private int numOfHits;
+
+    public String checkYourself(String guess) {
+    }
+
+    public void setLocationCells(int[] loc){
+    }
+}
