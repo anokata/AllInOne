cfca38fac3965811203921993e6e697287424161
anokata <anokata8@mail.ru>
2019-11-11 15:52:24 +0300

    sdc

--- java/2019/simpledotcomgame/SimpleDotCom.java
+++ java/2019/simpledotcomgame/SimpleDotCom.java
@@ -2,9 +2,60 @@ class SimpleDotCom {
     private int[] locationCells;
     private int numOfHits;
 
-    public String checkYourself(String guess) {
+    public String checkYourself(String strGuess) {
+        int guess = Integer.parseInt(strGuess);
+        String result = "Miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "Hit";
+                numOfHits++;
+                break;
+            }
+        }
+
+        if (numOfHits == locationCells.length) {
+            result = "Ruined";
+        }
+
+        System.out.println(result);
+        return result;
     }
 
     public void setLocationCells(int[] loc){
+        locationCells = loc;
+    }
+
+    private static void testOne() {
+        SimpleDotCom dot = new SimpleDotCom();
+        int[] loc = {2,3,4};
+        dot.setLocationCells(loc);
+        String guess = "2";
+        String result = dot.checkYourself(guess);
+        String testResult = "Fail";
+
+        if (result.equals("Hit")) {
+            testResult = "Pass";
+        }
+        System.out.println(testResult);
+    }
+
+    private static void testTwo() {
+        SimpleDotCom dot = new SimpleDotCom();
+        int[] loc = {2,3,4};
+        dot.setLocationCells(loc);
+        String result = dot.checkYourself("2");
+        result = dot.checkYourself("3");
+        result = dot.checkYourself("4");
+        String testResult = "Fail";
+
+        if (result.equals("Ruined")) {
+            testResult = "Pass";
+        }
+        System.out.println(testResult);
+    }
+    // Test
+    public static void main(String[] args) {
+        testOne();
+        testTwo();
     }
 }
---
+++ java/2019/simpledotcomgame/SimpleDotComGame.java
@@ -0,0 +1,34 @@
+class SimpleDotComGame {
+    /*
+     * создать SimpleDotCom
+     * выбрать случайную позицию и настроить созданный объект
+     * Пока не потоплено:
+     * Предлагаем сделать ход
+     * получаем ввод пользователя
+     * проверяем введённый ход
+     * даём объекту на проверку
+     * если попал то numOfGuesses++
+     * если потоплен то конец игры - вывод количества попыток
+     */
+
+    public static void main(String[] args) { 
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum+1, randomNum+2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+
+        while (isAlive) {
+            String guess = helper.getUserInput("Enter move:");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("Ruined")) {
+                isAlive = false;
+                System.out.println("You tried " + numOfGuesses + " tries.");
+            }
+        }
+    }
+}
