6e35af8a5a66809006b83b5fdc1b785f7e6672ff
anokata <anokata8@mail.ru>
2019-11-14 17:31:11 +0300

    her

---
+++ java/2019/heritage/Animal.java
@@ -0,0 +1,32 @@
+class Animal {
+    private int picture;
+    private String food;
+    private int hunger;
+    private long boundaries;
+    private Location location;
+
+    public void makeNoise() {
+    }
+
+    public void eat() {
+    }
+
+    public void sleep() {
+    }
+
+    public void roam() {
+    }
+
+}
+
+class Feline extends Animal {
+}
+
+class Canine extends Animal {
+}
+
+class Location {
+    public int x;
+    public int y;
+}
+
