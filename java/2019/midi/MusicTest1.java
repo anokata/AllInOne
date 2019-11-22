59327ecd9b6967a874b04c54726fe1d1591c1b43
anokata <anokata8@mail.ru>
2019-11-22 15:51:23 +0300

    midi

--- java/2019/midi/MusicTest1.java
+++ java/2019/midi/MusicTest1.java
@@ -4,18 +4,42 @@ import static java.lang.System.out;
 class MusicTest1 {
     public static void main(String[] args) {
         MusicTest1 mt = new MusicTest1();
-        handleRisk();
+        //handleRisk();
         out.println("start");
         mt.play();
         out.println("end");
     }
 
     public void play() {
+        Sequencer player;
         try {
-            Sequencer sequencer = MidiSystem.getSequencer();
+            player = MidiSystem.getSequencer();
             out.println("get it");
+            player.open();
+            out.println("opened");
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            out.println("created seq");
+            Track t = seq.createTrack();
+            out.println("created track");
+
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, 44, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            t.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            a.setMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(b, 2);
+            t.add(noteOff);
+
+            player.setSequence(seq);
+            out.println("created midi");
+            player.start();
+            out.println("played");
         } catch (MidiUnavailableException ex) {
             System.out.println("Fail");
+        } catch (InvalidMidiDataException ex) {
+            System.out.println("Fail data");
         }
     }
 
@@ -30,6 +54,9 @@ class MusicTest1 {
             nullrisk();
         } catch (NullPointerException e) {
             out.println("bad example");
+            return;
+        } finally {
+            out.println("Нисмотря ни на что! (даже return)");
         }
     }
 
