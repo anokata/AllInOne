d0085dcfce1092d3f00dba43ce3d50c19a8a4b5f
anokata <anokata8@mail.ru>
2019-11-22 10:14:59 +0300

    exceptions

--- java/2019/midi/MusicTest1.java
+++ java/2019/midi/MusicTest1.java
@@ -4,11 +4,41 @@ import static java.lang.System.out;
 class MusicTest1 {
     public static void main(String[] args) {
         MusicTest1 mt = new MusicTest1();
+        handleRisk();
+        out.println("start");
         mt.play();
+        out.println("end");
     }
 
     public void play() {
-        Sequencer sequencer = MidiSystem.getSequencer();
-        out.println("get it");
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            out.println("get it");
+        } catch (MidiUnavailableException ex) {
+            System.out.println("Fail");
+        }
+    }
+
+    public static void handleRisk() {
+        try {
+            risk();
+        } catch (Exception e) {
+            out.println("can't handle");
+        }
+
+        try {
+            nullrisk();
+        } catch (NullPointerException e) {
+            out.println("bad example");
+        }
+    }
+
+    public static void risk() throws Exception {
+        throw new Exception();
+    }
+
+    public static void nullrisk() {
+        Object o = null;
+        o.getClass();
     }
 }
