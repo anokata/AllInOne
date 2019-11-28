08a294dbd07f7632fc6c1cd17735395a87f8cdf3
anokata <anokata8@mail.ru>
2019-11-28 19:51:14 +0300

    read

--- java/2019/FileIO.java
+++ java/2019/FileIO.java
@@ -1,10 +1,30 @@
 import java.io.*;
 class FileIO {
     public static void main(String[] args) {
+        writeTest();
+        reatTest();
+    }
+
+    static void writeTest() {
         try {
             FileWriter f = new FileWriter("tstjava");
             f.write("hi file");
             f.close();
+        } catch (IOException e) {e.printStackTrace();}
+    }
+
+    static void reatTest() {
+        try {
+            File file = new File("FileIO.java");
+            BufferedReader in = new BufferedReader(new FileReader(file));
+
+            String line = null;
+
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            in.close();
         } catch (IOException e) {e.printStackTrace();}
     }
 
