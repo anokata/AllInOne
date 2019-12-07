19c457c533cc0b6037c997c2ba75121c6c9a96c9
anokata <anokata8@mail.ru>
2019-12-07 10:53:03 +0300

    gen

--- java/2019/GenericOne.java
+++ java/2019/GenericOne.java
@@ -10,11 +10,34 @@ class GenericOne {
         Creature[] creaturesArray = {new Creature(), new Man(), new Pet()};
         viewArray(creaturesArray);
 
+        ArrayList<Creature> ac = new ArrayList<Creature>();
+        //ArrayList<Creature> ac = new ArrayList<Homo>(); // NOT
+        ac.add(new Homo());
+        ac.add(new Man());
+        viewAList(ac);
 
+        //ArrayList<Homo> ah = new ArrayList<Creature>(); // NOT
+        List<Creature> lc = new ArrayList<Creature>();
+        ArrayList<Homo> ah = new ArrayList<Homo>();
+        // ac = ah; NOT
+        lc = ac;
+
+        ah.add(new Homo());
+        ah.add(new Man());
+        viewHList(ah);
+        // viewAList(ah); NOT
     }
 
     void viewArray(Creature[] a) {
         for (Creature x : a) { x.eat(); }
+    }
+
+    void viewAList(ArrayList<Creature> l) {
+        for (Creature x : l) { x.eat(); }
+    }
+
+    <T extends Homo> void viewHList(ArrayList<T> l) {
+        for (T x : l) { x.eat(); }
     }
 
 }
