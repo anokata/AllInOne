17722f9f6181257f7a084793e303182c6e7a1494
anokata <anokata8@mail.ru>
2019-12-23 09:14:32 +0300

    cmd

---
+++ java/2019/pat/command/Light.java
@@ -0,0 +1,18 @@
+class Light {
+    public static void main(String[] args) {
+        Light app = new Light();
+    }
+
+    Light () {
+        System.out.println("Created Light");
+    }
+
+    public void on() {
+        System.out.println("Light is on");
+    }
+
+    public void off() {
+        System.out.println("Light is off");
+    }
+}
+
