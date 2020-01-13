12c45d3bb71e1d64f11ef2154ee19e7b34d48380
anokata <anokata8@mail.ru>
2020-01-13 15:27:08 +0300

    beatmodel

--- java/2019/pat/mvc/BeatModel.java
+++ java/2019/pat/mvc/BeatModel.java
@@ -16,9 +16,6 @@ class BeatModel implements BeatModelInterface, MetaEventListener {
     BeatModel () {
     }
 
-    void buildTrackAndStart() {
-    }
-
     public void initialize() {
         setUpMidi();
         buildTrackAndStart();
@@ -94,8 +91,42 @@ class BeatModel implements BeatModelInterface, MetaEventListener {
             sequencer = MidiSystem.getSequencer();
             sequencer.open();
             sequencer.addMetaEventListener(this);
-
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(getBPM());
+            sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
         } catch (Exception ex) { ex.printStackTrace(); }
+    }
+
+    public void buildTrackAndStart() {
+        int[] trackList = {35, 0, 46, 0};
+        sequence.deleteTrack(null);
+        track = sequence.createTrack();
+        makeTracks(trackList);
+        track.add(makeEvent(192,9,1,0,4));
+        try {
+            sequencer.setSequence(sequence);
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+
+    public void makeTracks(int[] list) {
+        for (int i=0; i<list.length; i++) {
+            int key = list[i];
+            if (key != 0) {
+                track.add(makeEvent(144,9,key,100,i));
+                track.add(makeEvent(128,9,key,100,i+1));
+            }
+        }
+    }
+
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception ex) { ex.printStackTrace(); }
+        return event;
     }
 
 }
