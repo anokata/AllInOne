70d011ee649d4a303120585997171424cdf6ae50
anokata <anokata8@mail.ru>
2020-01-15 11:51:17 +0300

    servlet...

---
+++ java/2019/pat/mvc/DJViewServlet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+//import javax.servlet.*;
+
+class DJViewServlet extends HttpServlet {
+    public static void main(String[] args) {
+        DJViewServlet app = new DJViewServlet();
+    }
+
+    DJViewServlet () {
+        System.out.println("Created DJViewServlet");
+    }
+}
+
